4229bd7b2cb9199f2a1364dc2f32d8412f2d49d3
Sergey Nikomarov <snikomarov@acumatica.com>
2024-03-03 17:21:55 +0100

    ATR-785: ported constants from core compatibilyzer

---
+++ src/Acuminator/Acuminator.Runner.NetFramework/Constants/CommandLineArgNames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acuminator.Runner.Constants
+{
+	internal static class CommandLineArgNames
+	{
+		public const string CodeSource = "codeSource";
+
+		//public const char VerbosityShort = 'v';
+		//public const string VerbosityLong = "verbosity";
+
+		public const string DisableSuppressionMechanism = "noSuppression";
+		public const string MSBuildPath = "msBuildPath";
+
+		public const string OutputAbsolutePathsToUsages = "outputAbsolutePaths";
+
+		public const char OutputFileShort = 'f';
+		public const string OutputFileLong = "file";
+
+		public const string OutputFormat = "format";
+	}
+}
---
+++ src/Acuminator/Acuminator.Runner.NetFramework/Constants/CommonConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Acuminator.Runner.Constants
+{
+	internal static class CommonConstants
+	{
+		public const string ProjectFileExtension = ".csproj";
+		public const string SolutionFileExtension = ".sln";
+	}
+}
