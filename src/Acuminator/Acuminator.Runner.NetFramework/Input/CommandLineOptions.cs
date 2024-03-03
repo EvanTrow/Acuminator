2e530b434453ee931dd95e26fc8b03f581a84734
Sergey Nikomarov <snikomarov@acumatica.com>
2024-03-03 17:24:19 +0100

    ATR-785: ported more files from core compatibility

---
+++ src/Acuminator/Acuminator.Runner.NetFramework/Input/CommandLineOptions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Serilog.Events;
+
+using CommandLine;
+
+using Acuminator.Runner.Constants;
+
+namespace Acuminator.Runner.Input
+{
+	internal class CommandLineOptions
+	{
+		/// <summary>
+		/// The code source that will be analysed by Acuminator.
+		/// </summary>
+		/// <remarks>
+		/// Currently, the supported code sources are C# projects and C# solutions.
+		/// </remarks>
+		[Value(index: 0, MetaName = CommandLineArgNames.CodeSource, Required = true,
+			   HelpText = "A path to the \"code source\" which will be validated. The term \"code source\" is a generalization for components/services that can provide source code to the tool.\n" +
+						  "Currently, the supported code sources are C# projects and C# solutions.")]
+		public string CodeSource { get; }
+
+		///// <summary>
+		///// Optional explicitly specified logger <see cref="LogEventLevel"/> verbosity for tool's own messages. <br/>
+		///// If null then <see cref="LogEventLevel.Information"/> will be used as default.
+		///// </summary>
+		///// <value>
+		///// The explicitly specified logger's verbosity.
+		///// </value>
+		//[Option(shortName: CommandLineArgNames.VerbosityShort, longName: CommandLineArgNames.VerbosityLong,
+		//		HelpText = "This optional parameter allows you to explicitly specify logger verbosity. The allowed values are taken from the \"" +
+		//				  $"{nameof(Serilog)}.{nameof(Serilog.Events)}.{nameof(Serilog.Events.LogEventLevel)} enum.\n\n" +
+		//				   "The allowed values:\n" +
+		//				  $"{nameof(LogEventLevel.Verbose)}, {nameof(LogEventLevel.Debug)}, {nameof(LogEventLevel.Information)}, " +
+		//				  $"{nameof(LogEventLevel.Warning)}, {nameof(LogEventLevel.Error)}, {nameof(LogEventLevel.Fatal)}.\n\n" +
+		//				  $"By default, the logger will use the \"{nameof(LogEventLevel.Information)}\" verbosity.")]
+		//public string? Verbosity { get; }
+
+		/// <summary>
+		/// If this flag is set to true then the code analysis won't take into consideration suppression comments present in the code.
+		/// </summary>
+		[Option(longName: CommandLineArgNames.DisableSuppressionMechanism,
+				HelpText = "When this optional flag is specified, the code analysis report suppressed Acuminator errors.")]
+		public bool DisableSuppressionMechanism { get; }
+
+		/// <summary>
+		/// Optional explicitly specified path to MSBuild. Can be null. If null then MSBuild path is retrieved automatically.
+		/// </summary>
+		/// <value>
+		/// The optional explicitly specified path to MSBuild.
+		/// </value>
+		[Option(longName: CommandLineArgNames.MSBuildPath,
+				HelpText = "This optional parameter allows you to provide explicitly a path to the MSBuild tool that will be used for analysis.\n" +
+						   "By default, MSBuild installations will be searched automatically on the current machine and the latest found version will be used.")]
+		public string? MSBuildPath { get; }
+
+		/// <summary>
+		/// The name of the output file. If not specified then the report will be outputted to the console window.
+		/// </summary>
+		[Option(shortName: CommandLineArgNames.OutputFileShort, longName: CommandLineArgNames.OutputFileLong,
+				HelpText = "The name of the output file. If not specified then the report will be outputted to the console window.")]
+		public string? OutputFileName { get; }
+
+		/// <summary>
+		/// This flag regulates how the locations of API usages will be output.<br/>
+		///	By default, file paths in locations are relative to the containing project directory. However, if this flag is set then the absolute file paths will be used.
+		/// </summary>
+		[Option(longName: CommandLineArgNames.OutputAbsolutePathsToUsages,
+				HelpText = "This flag regulates how the locations of API usages will be output.\n" +
+						   "By default, file paths in locations are relative to the containing project directory. " +
+						   "However, if this flag is set then the absolute file paths will be used.\n")]
+		public bool OutputAbsolutePathsToUsages { get; }
+
+		/// <summary>
+		/// The report output format. There are two supported values:
+		/// <list type="bullet">
+		/// <item>"text" to output the report in plain text, this is the default output mode,</item>
+		/// <item>"json" to output the report in JSON format.</item>
+		/// </list>
+		/// </summary>
+		[Option(longName: CommandLineArgNames.OutputFormat,
+				HelpText = "The report output format. There are two supported values:\n" +
+						   "- \"text\" to output the report in plain text, this is the default output mode,\n" +
+						   "- \"json\" to output the report in JSON format.")]
+		public string? OutputFormat { get; }
+
+		// Constructor arguments order must be the same as the properties order. This allows command line parser to initialize immutable options object via constructor.
+		// See this for details: https://github.com/commandlineparser/commandline/wiki/Immutable-Options-Type
+		public CommandLineOptions(string codeSource, bool disableSuppressionMechanism, string? msBuildPath, bool includeApiUsages, bool includeAllDistinctApis,
+								  bool showMembersOfUsedType, string? reportGrouping, string? outputFileName, bool outputAbsolutePathsToUsages, string? outputFormat)
+		{
+			CodeSource 					= codeSource;
+			//Verbosity 					= verbosity;
+			DisableSuppressionMechanism = disableSuppressionMechanism;
+			MSBuildPath 				= msBuildPath;
+			OutputFileName				= outputFileName;
+			OutputAbsolutePathsToUsages = outputAbsolutePathsToUsages;
+			OutputFormat				= outputFormat;
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Runner.NetFramework/RunResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Acuminator.Runner.NetFramework
+{
+    /// <summary>
+    /// Run result
+    /// </summary>
+    internal enum RunResult
+    {
+        /// <summary>
+        /// The run finished successfully.
+        /// </summary>
+        Success = 0,
+
+		/// <summary>
+		/// The run finished successfully but the analyzed code source did not pass the validation.
+		/// </summary>
+		RequirementsNotMet = 1,
+
+		/// <summary>
+		/// The run was cancelled.
+		/// </summary>
+		Cancelled = 2,
+
+		/// <summary>
+		/// The run was interrupted by a runtime error.
+		/// </summary>
+		RunTimeError = 4
+    }
+
+    /// <summary>
+    /// The helper class for <see cref="RunResult"/>.
+    /// </summary>
+    internal static class RunResultHelper
+    {
+		public static bool IsError(this RunResult result) => 
+			result != RunResult.Success && result != RunResult.Cancelled;
+
+		public static bool IsCancelled(this RunResult result) => result == RunResult.Cancelled;
+
+		public static RunResult Combine(this RunResult x, RunResult y) =>
+		   x >= y ? x : y;
+
+		public static int ToExitCode(this RunResult result) => (int)result;
+    }
+}
