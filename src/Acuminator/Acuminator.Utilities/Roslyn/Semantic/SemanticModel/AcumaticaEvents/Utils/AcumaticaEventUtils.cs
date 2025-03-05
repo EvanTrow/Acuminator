89f169e0b736b1e27eac9ded8146d2c0cc28daf9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-03-05 04:19:40 +0100

    ATR-855: optimized slightly calculation of the target kind

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/AcumaticaEvents/Utils/AcumaticaEventUtils.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/AcumaticaEvents/Utils/AcumaticaEventUtils.cs
@@ -18,12 +18,37 @@ namespace Acuminator.Utilities.Roslyn.Semantic.AcumaticaEvents
 		/// <returns>
 		/// The event target kind from the given event type.
 		/// </returns>
-		public static EventTargetKind GetEventTargetKindByEventType(this EventType eventType) =>
-			eventType.IsDacRowEvent()
-				? EventTargetKind.Row
-				: eventType.IsDacFieldEvent()
-					? EventTargetKind.Field
-					: EventTargetKind.None;
+		public static EventTargetKind GetEventTargetKindByEventType(this EventType eventType)
+		{
+			switch (eventType)
+			{
+				case EventType.FieldSelecting:
+				case EventType.FieldDefaulting:
+				case EventType.FieldVerifying:
+				case EventType.FieldUpdating:
+				case EventType.FieldUpdated:
+				case EventType.CacheAttached:
+				case EventType.CommandPreparing:
+				case EventType.ExceptionHandling:
+					return EventTargetKind.Field;
+
+				case EventType.RowSelecting:
+				case EventType.RowSelected:
+				case EventType.RowInserting:
+				case EventType.RowInserted:
+				case EventType.RowUpdating:
+				case EventType.RowUpdated:
+				case EventType.RowDeleting:
+				case EventType.RowDeleted:
+				case EventType.RowPersisting:
+				case EventType.RowPersisted:
+					return EventTargetKind.Row;
+
+				case EventType.None:
+				default:
+					return EventTargetKind.None;
+			}
+		}
 
 		/// <summary>
 		/// Check if <paramref name="eventType"/> is DAC field event.
