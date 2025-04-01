4f529cbfe0a7c30bce101b656cdd303b68ccaa75
Sergey Nikomarov <snikomarov@acumatica.com>
2025-04-01 04:05:20 +0200

    ATR-855: renamed old storage into AllGraphEventHandlerOverridesChainsStorage

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/PXGraphWithEvents/Storage/PXGraphEventSemanticModel.AllGraphEventHandlerOverridesChainsStorage.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.AcumaticaEvents;
+
+using Microsoft.CodeAnalysis;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
+{
+	public partial class PXGraphEventSemanticModel
+	{
+		/// <inheritdoc cref="IGraphEventHandlerOverridesChainsStorage"/>
+		private class AllGraphEventHandlerOverridesChainsStorage : IGraphEventHandlerOverridesChainsStorage
+		{
+			public int AllEventHandlersCount { get; }
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowSelectingByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowSelectingEvents => RowSelectingByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowSelectedByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowSelectedEvents => RowSelectedByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowInsertingByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowInsertingEvents => RowInsertingByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowInsertedByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowInsertedEvents => RowInsertedByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowUpdatingByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowUpdatingEvents => RowUpdatingByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowUpdatedByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowUpdatedEvents => RowUpdatedByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowDeletingByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowDeletingEvents => RowDeletingByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowDeletedByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowDeletedEvents => RowDeletedByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowPersistingByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowPersistingEvents => RowPersistingByName.Values;
+
+			public ImmutableDictionary<string, GraphRowEventHandlerInfo> RowPersistedByName { get; }
+			public IEnumerable<GraphRowEventHandlerInfo> RowPersistedEvents => RowPersistedByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> FieldSelectingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> FieldSelectingEvents => FieldSelectingByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> FieldDefaultingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> FieldDefaultingEvents => FieldDefaultingByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> FieldVerifyingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> FieldVerifyingEvents => FieldVerifyingByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> FieldUpdatingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> FieldUpdatingEvents => FieldUpdatingByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> FieldUpdatedByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> FieldUpdatedEvents => FieldUpdatedByName.Values;
+
+			public ImmutableDictionary<string, GraphCacheAttachedEventHandlerInfo> CacheAttachedByName { get; }
+			public IEnumerable<GraphCacheAttachedEventHandlerInfo> CacheAttachedEvents => CacheAttachedByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> CommandPreparingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> CommandPreparingEvents => CommandPreparingByName.Values;
+
+			public ImmutableDictionary<string, GraphFieldEventHandlerInfo> ExceptionHandlingByName { get; }
+			public IEnumerable<GraphFieldEventHandlerInfo> ExceptionHandlingEvents => ExceptionHandlingByName.Values;
+
+			public AllGraphEventHandlerOverridesChainsStorage(ImmutableDictionary<string, GraphRowEventHandlerInfo> rowSelectingByName, 
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowSelectedByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowInsertingByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowInsertedByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowUpdatingByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowUpdatedByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowDeletingByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowDeletedByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowPersistingByName,
+											 ImmutableDictionary<string, GraphRowEventHandlerInfo> rowPersistedByName,
+
+											 ImmutableDictionary<string, GraphCacheAttachedEventHandlerInfo> cacheAttachedByName,
+
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldSelectingByName,
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldDefaultingByName,
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldVerifyingByName,
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldUpdatingByName,
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldUpdatedByName,
+
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> commandPreparingByName,
+											 ImmutableDictionary<string, GraphFieldEventHandlerInfo> exceptionHandlingByName)
+			{
+				RowSelectingByName = rowSelectingByName;
+				RowSelectedByName = rowSelectedByName;
+
+				RowInsertingByName = rowInsertingByName;
+				RowInsertedByName = rowInsertedByName;
+
+				RowUpdatingByName = rowUpdatingByName;
+				RowUpdatedByName = rowUpdatedByName;
+
+				RowDeletingByName = rowDeletingByName;
+				RowDeletedByName = rowDeletedByName;
+
+				RowPersistingByName = rowPersistingByName;
+				RowPersistedByName = rowPersistedByName;
+
+				CacheAttachedByName = cacheAttachedByName;
+
+				FieldSelectingByName  = fieldSelectingByName;
+				FieldDefaultingByName = fieldDefaultingByName;
+				FieldVerifyingByName  = fieldVerifyingByName;
+				FieldUpdatingByName   = fieldUpdatingByName;
+				FieldUpdatedByName 	  = fieldUpdatedByName;
+
+				CommandPreparingByName = commandPreparingByName;
+				ExceptionHandlingByName = exceptionHandlingByName;
+
+				AllEventHandlersCount = RowSelectingByName.Count   + RowSelectedByName.Count	 + RowInsertingByName.Count		+ RowInsertedByName.Count +
+										RowUpdatingByName.Count    + RowUpdatedByName.Count		 + RowDeletingByName.Count		+ RowDeletedByName.Count +
+										RowPersistingByName.Count  + RowPersistedByName.Count    + CacheAttachedByName.Count	+
+										FieldSelectingByName.Count + FieldDefaultingByName.Count + FieldVerifyingByName.Count   +
+										FieldUpdatingByName.Count  + FieldUpdatedByName.Count	 + CommandPreparingByName.Count + ExceptionHandlingByName.Count;
+			}
+
+			public static IGraphEventHandlersStorage FromCollected(NaturalOrderEventHandlersCollector eventsCollector) =>
+				new GraphEventHandlersStorage(
+					 rowSelectingByName: GetRowEventHandlers(eventsCollector, EventType.RowSelecting),
+		 			 rowSelectedByName: GetRowEventHandlers(eventsCollector, EventType.RowSelected),
+
+			 		 rowInsertingByName: GetRowEventHandlers(eventsCollector, EventType.RowInserting),
+		 			 rowInsertedByName: GetRowEventHandlers(eventsCollector, EventType.RowInserted),
+
+		 			 rowUpdatingByName: GetRowEventHandlers(eventsCollector, EventType.RowUpdating),
+					 rowUpdatedByName: GetRowEventHandlers(eventsCollector, EventType.RowUpdated),
+
+		 			 rowDeletingByName: GetRowEventHandlers(eventsCollector, EventType.RowDeleting),
+					 rowDeletedByName: GetRowEventHandlers(eventsCollector, EventType.RowDeleted),
+
+				 	 rowPersistingByName: GetRowEventHandlers(eventsCollector, EventType.RowPersisting),
+			 		 rowPersistedByName: GetRowEventHandlers(eventsCollector, EventType.RowPersisted),
+
+				 	 cacheAttachedByName: GetCacheAttachedEventHandlers(eventsCollector),
+
+					 fieldSelectingByName: GetFieldEventHandlers(eventsCollector, EventType.FieldSelecting),
+					 fieldDefaultingByName: GetFieldEventHandlers(eventsCollector, EventType.FieldDefaulting),
+					 fieldVerifyingByName: GetFieldEventHandlers(eventsCollector, EventType.FieldVerifying),
+				 	 fieldUpdatingByName: GetFieldEventHandlers(eventsCollector, EventType.FieldUpdating),
+			 		 fieldUpdatedByName: GetFieldEventHandlers(eventsCollector, EventType.FieldUpdated),
+
+					 commandPreparingByName: GetFieldEventHandlers(eventsCollector, EventType.CommandPreparing),
+					 exceptionHandlingByName: GetFieldEventHandlers(eventsCollector, EventType.ExceptionHandling)
+					);
+
+			public static IGraphEventHandlersStorage FromEventHandlersInNaturalCollectionOrder(INamedTypeSymbol containingType,
+																							IGraphEventHandlersStorage allEventHandlers) =>
+				new GraphEventHandlersStorage(
+						 rowSelectingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowSelectingByName),
+						 rowSelectedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowSelectedByName),
+
+						 rowInsertingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowInsertingByName),
+						 rowInsertedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowInsertedByName),
+
+						 rowUpdatingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowUpdatingByName),
+						 rowUpdatedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowUpdatedByName),
+
+						 rowDeletingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowDeletingByName),
+						 rowDeletedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowDeletedByName),
+
+						 rowPersistingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowPersistingByName),
+						 rowPersistedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.RowPersistedByName),
+
+						 cacheAttachedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.CacheAttachedByName),
+
+						 fieldSelectingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.FieldSelectingByName),
+						 fieldDefaultingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.FieldDefaultingByName),
+						 fieldVerifyingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.FieldVerifyingByName),
+						 fieldUpdatingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.FieldUpdatingByName),
+						 fieldUpdatedByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.FieldUpdatedByName),
+
+						 commandPreparingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.CommandPreparingByName),
+						 exceptionHandlingByName: GetDeclaredEventHandlersInNaturalOrder(containingType, allEventHandlers.ExceptionHandlingByName)
+						);
+
+			public IEnumerable<GraphEventHandlerInfoBase> GetEventHandlersByEventType(EventType eventType) => eventType switch
+			{
+				EventType.RowSelecting 	=> RowSelectingEvents,
+				EventType.RowSelected 	=> RowSelectedEvents,
+				EventType.RowInserting 	=> RowInsertingEvents,
+				EventType.RowInserted 	=> RowInsertedEvents,
+				EventType.RowUpdating 	=> RowUpdatingEvents,
+				EventType.RowUpdated 	=> RowUpdatedEvents,
+				EventType.RowDeleting 	=> RowDeletingEvents,
+				EventType.RowDeleted 	=> RowDeletedEvents,
+				EventType.RowPersisting => RowPersistingEvents,
+				EventType.RowPersisted 	=> RowPersistedEvents,
+
+				EventType.FieldSelecting  => FieldSelectingEvents,
+				EventType.FieldDefaulting => FieldDefaultingEvents,
+				EventType.FieldVerifying  => FieldVerifyingEvents,
+				EventType.FieldUpdating   => FieldUpdatingEvents,
+				EventType.FieldUpdated 	  => FieldUpdatedEvents,
+
+				EventType.CacheAttached 	=> CacheAttachedEvents,
+				EventType.CommandPreparing 	=> CommandPreparingEvents,
+				EventType.ExceptionHandling => ExceptionHandlingEvents,
+				EventType.None 				=> [],
+				_ 							=> throw new NotSupportedException($"Event type {eventType} is not supported")
+			};
+
+			public IEnumerable<GraphEventHandlerInfoBase> GetAllEventHandlers()
+			{
+				IEnumerable<GraphEventHandlerInfoBase>? allEvents = RowSelectingByName.Values;
+
+				AppendRowEvents(RowSelectedByName);
+				AppendRowEvents(RowInsertingByName);
+				AppendRowEvents(RowInsertedByName);
+				AppendRowEvents(RowUpdatingByName);
+				AppendRowEvents(RowUpdatedByName);
+				AppendRowEvents(RowDeletingByName);
+				AppendRowEvents(RowDeletedByName);
+				AppendRowEvents(RowPersistingByName);
+				AppendRowEvents(RowPersistedByName);
+
+				AppendFieldEvents(FieldSelectingByName);
+				AppendFieldEvents(FieldDefaultingByName);
+				AppendFieldEvents(FieldVerifyingByName);
+				AppendFieldEvents(FieldUpdatingByName);
+				AppendFieldEvents(FieldUpdatedByName);
+
+				if (CacheAttachedByName.Count > 0)
+					allEvents = allEvents.Concat(CacheAttachedByName.Values);
+
+				AppendFieldEvents(CommandPreparingByName);
+				AppendFieldEvents(ExceptionHandlingByName);
+
+				return allEvents;
+
+				//------------------------------------Local Function----------------------------------------------
+				void AppendRowEvents(ImmutableDictionary<string, GraphRowEventHandlerInfo> rowEvents)
+				{
+					if (rowEvents.Count > 0)
+						allEvents = allEvents.Concat(rowEvents.Values);
+				}
+
+				void AppendFieldEvents(ImmutableDictionary<string, GraphFieldEventHandlerInfo> fieldEvents)
+				{
+					if (fieldEvents.Count > 0)
+						allEvents = allEvents.Concat(fieldEvents.Values);
+				}
+			}
+
+			private static ImmutableDictionary<string, GraphRowEventHandlerInfo> GetRowEventHandlers(NaturalOrderEventHandlersCollector eventsCollector, EventType eventType)
+			{
+				OverridableItemsCollection<GraphRowEventHandlerInfo>? rawCollection = eventsCollector.GetRowEventHandlers(eventType);
+				return rawCollection?.ToImmutableDictionary() ?? ImmutableDictionary<string, GraphRowEventHandlerInfo>.Empty;
+			}
+
+			private static ImmutableDictionary<string, GraphFieldEventHandlerInfo> GetFieldEventHandlers(NaturalOrderEventHandlersCollector eventsCollector, EventType eventType)
+			{
+				OverridableItemsCollection<GraphFieldEventHandlerInfo>? rawCollection = eventsCollector.GetFieldEventHandlers(eventType);
+				return rawCollection?.ToImmutableDictionary() ?? ImmutableDictionary<string, GraphFieldEventHandlerInfo>.Empty;
+			}
+
+			private static ImmutableDictionary<string, GraphCacheAttachedEventHandlerInfo> GetCacheAttachedEventHandlers(NaturalOrderEventHandlersCollector eventsCollector)
+			{
+				OverridableItemsCollection<GraphCacheAttachedEventHandlerInfo>? rawCollection = eventsCollector.CacheAttachedEventHandlers;
+				return rawCollection?.ToImmutableDictionary() ?? ImmutableDictionary<string, GraphCacheAttachedEventHandlerInfo>.Empty;
+			}
+
+			private static ImmutableDictionary<string, THandler> GetDeclaredEventHandlersInNaturalOrder<THandler>(INamedTypeSymbol containingType,
+																								ImmutableDictionary<string, THandler> handlersOfSameEventType)
+			where THandler : GraphEventHandlerInfoBase<THandler>
+			{
+				if (handlersOfSameEventType.Count == 0)
+					return handlersOfSameEventType;
+
+				// HOT PATH optimization - if all handlers in the source dictionary are declared in the containing type, return the source dictionary
+				bool sourceHasNotDeclaredHandlers = handlersOfSameEventType.Values.Any(handler => !handler.Symbol.IsDeclaredInType(containingType));
+
+				if (!sourceHasNotDeclaredHandlers)
+					return handlersOfSameEventType;
+
+				// For natural order it is enough to check the most overriden handler. If it is not declared in the containing type, 
+				// then all its base handlers are not declared in the containing type too.
+				var declaredEventHandlers = handlersOfSameEventType.Where(pair => pair.Value.Symbol.IsDeclaredInType(containingType))
+																   .ToImmutableDictionary(keyComparer: handlersOfSameEventType.KeyComparer);
+				return declaredEventHandlers;
+			}
+
+			/// <summary>
+			/// Gets event handlers in Acumatica collection order. Left as is for the possible future usage if support of Acumatica event handlers order will be needed. 
+			/// </summary>
+			private static ImmutableDictionary<string, THandler> GetDeclaredEventHandlersInAcumaticaCollectionOrder<THandler>(INamedTypeSymbol containingType,
+																								ImmutableDictionary<string, THandler> handlersOfSameEventType)
+			where THandler : GraphEventHandlerInfoBase<THandler>
+			{
+				if (handlersOfSameEventType.Count == 0)
+					return handlersOfSameEventType;
+
+				// HOT PATH optimization - if all handlers in the source dictionary are declared in the containing type, return the source dictionary
+				bool sourceHasNotDeclaredHandlers = handlersOfSameEventType.Values.Any(handler => !handler.Symbol.IsDeclaredInType(containingType));
+
+				if (!sourceHasNotDeclaredHandlers)
+					return handlersOfSameEventType;
+
+				var declaredEventHandlers = new Dictionary<string, THandler>(handlersOfSameEventType.Count, handlersOfSameEventType.KeyComparer);			
+
+				foreach (var (key, eventHandler) in handlersOfSameEventType)
+				{
+					if (eventHandler.Symbol.IsDeclaredInType(containingType))
+					{
+						declaredEventHandlers.Add(key, eventHandler);
+						continue;
+					}
+
+					if (eventHandler.Base == null)     // handler is not declared in type at all
+						continue;
+
+					var baseHandlers = eventHandler.JustOverridenItems();
+					THandler? declaredBaseHandler = baseHandlers.FirstOrDefault(h => h.Symbol.IsDeclaredInType(containingType));
+
+					if (declaredBaseHandler == null)    // handler is not declared in type at all
+						continue;
+
+					declaredEventHandlers.Add(key, declaredBaseHandler);
+				}
+
+				return declaredEventHandlers.ToImmutableDictionary();
+			}
+		}
+	}
+}
