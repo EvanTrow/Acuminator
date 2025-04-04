b9c5b6b9ecb8e93eb384030f3116ea6ec7c7ea8a
Sergey Nikomarov <snikomarov@acumatica.com>
2025-04-04 14:14:25 +0200

    ATR-866: reworked data type attributes on a property info

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/CategorizedAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Acuminator.Utilities.Roslyn.Semantic.Attribute;
-
-namespace Acuminator.Utilities.Roslyn.PXFieldAttributes.Infos;
-
-public record CategorizedAttributes(
-	List<DacFieldAttributeInfo>? TypeAttributesOnDacProperty, 
-	List<DacFieldAttributeInfo>? TypeAttributesWithDifferentDataTypesOnAggregator, 
-	bool HasNonNullDataType);
---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/DataTypeAttributesOnDacProperty.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Attribute;
+
+namespace Acuminator.Utilities.Roslyn.PXFieldAttributes.Infos;
+
+public class DataTypeAttributesOnDacProperty
+{
+	public ImmutableArray<DacFieldAttributeInfo> AllDataTypeAttributesOnDacProperty { get; }
+
+	public ImmutableArray<DacFieldAttributeInfo> DataTypeAttributesWithMultipleAggregatedDataTypes { get; }
+
+	public bool HasNonNullDataType { get; }
+
+	public DataTypeAttributesOnDacProperty(IEnumerable<DacFieldAttributeInfo>? datatypeAttributesOnDacProperty,
+										   IEnumerable<DacFieldAttributeInfo>? dataTypeAttributesWithMultipleAggregatedDataTypes,
+										   bool hasNonNullDataType)
+	{
+		AllDataTypeAttributesOnDacProperty = datatypeAttributesOnDacProperty?.ToImmutableArray() ?? ImmutableArray<DacFieldAttributeInfo>.Empty;
+		DataTypeAttributesWithMultipleAggregatedDataTypes = dataTypeAttributesWithMultipleAggregatedDataTypes?.ToImmutableArray() ?? 
+															ImmutableArray<DacFieldAttributeInfo>.Empty;
+		HasNonNullDataType = hasNonNullDataType;
+	}
+}
