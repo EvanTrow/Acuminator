2db7f11a11cb6235efcec15a820540ed77296ae5
Sergey Nikomarov <snikomarov@acumatica.com>
2025-04-06 01:28:57 +0200

    ATR-866: moved creation logic for DataTypeAttributesOnDacProperty to the type itself

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/DataTypeAttributesOnDacProperty.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/DataTypeAttributesOnDacProperty.cs
@@ -1,26 +1,77 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn.Semantic.Attribute;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+using Microsoft.CodeAnalysis;
 
 namespace Acuminator.Utilities.Roslyn.PXFieldAttributes.Infos;
 
-public class DataTypeAttributesOnDacProperty
+public sealed class DataTypeAttributesOnDacProperty
 {
-	public ImmutableArray<DacFieldAttributeInfo> AllDataTypeAttributesOnDacProperty { get; }
+	public ImmutableArray<DacFieldAttributeInfo> AllDeclaredDatatypeAttributesOnDacProperty { get; }
 
-	public ImmutableArray<DacFieldAttributeInfo> DataTypeAttributesWithMultipleAggregatedDataTypes { get; }
+	public ImmutableArray<DacFieldAttributeInfo> DeclaredDataTypeAttributesWithMultipleAggregatedDataTypes { get; }
 
 	public bool HasNonNullDataType { get; }
 
-	public DataTypeAttributesOnDacProperty(IEnumerable<DacFieldAttributeInfo>? datatypeAttributesOnDacProperty,
-										   IEnumerable<DacFieldAttributeInfo>? dataTypeAttributesWithMultipleAggregatedDataTypes,
-										   bool hasNonNullDataType)
+	private DataTypeAttributesOnDacProperty(IEnumerable<DacFieldAttributeInfo>? allDeclaredDatatypeAttributesOnDacProperty,
+											IEnumerable<DacFieldAttributeInfo>? declaredDataTypeAttributesWithMultipleAggregatedDataTypes,
+											bool hasNonNullDataType)
 	{
-		AllDataTypeAttributesOnDacProperty = datatypeAttributesOnDacProperty?.ToImmutableArray() ?? ImmutableArray<DacFieldAttributeInfo>.Empty;
-		DataTypeAttributesWithMultipleAggregatedDataTypes = dataTypeAttributesWithMultipleAggregatedDataTypes?.ToImmutableArray() ?? 
-															ImmutableArray<DacFieldAttributeInfo>.Empty;
+		AllDeclaredDatatypeAttributesOnDacProperty = allDeclaredDatatypeAttributesOnDacProperty?.ToImmutableArray() ?? 
+													 ImmutableArray<DacFieldAttributeInfo>.Empty;
+		DeclaredDataTypeAttributesWithMultipleAggregatedDataTypes = declaredDataTypeAttributesWithMultipleAggregatedDataTypes?.ToImmutableArray() ?? 
+																	ImmutableArray<DacFieldAttributeInfo>.Empty;
 		HasNonNullDataType = hasNonNullDataType;
+	}
+
+	public static DataTypeAttributesOnDacProperty CollectDataTypeAttributesFromDacProperty(DacPropertyInfo property)
+	{
+		var declaredAttributesWithFieldTypeMetadata = property.CheckIfNull()
+															  .Attributes
+															  .Where(aInfo => !aInfo.AggregatedAttributeMetadata.IsDefaultOrEmpty)
+															  .ToList(capacity: property.Attributes.Length);
+
+		List<DacFieldAttributeInfo>? allDeclaredDatatypeAttributesOnDacProperty = null;
+		List<DacFieldAttributeInfo>? declaredDataTypeAttributesWithMultipleAggregatedDataTypes = null;
+		bool hasNonNullDataType = false;
+
+		foreach (var attributeDeclaredOnProperty in declaredAttributesWithFieldTypeMetadata)
+		{
+			var aggregatedDataTypeAttributes = attributeDeclaredOnProperty.AggregatedAttributeMetadata
+																		  .Where(atrMetadata => atrMetadata.IsFieldAttribute)
+																		  .ToList(capacity: 2);
+			if (aggregatedDataTypeAttributes.Count == 0)
+				continue;
+
+			allDeclaredDatatypeAttributesOnDacProperty ??= new List<DacFieldAttributeInfo>(capacity: 2);
+			allDeclaredDatatypeAttributesOnDacProperty.Add(attributeDeclaredOnProperty);
+
+			// HOT PATH optimization 
+			if (aggregatedDataTypeAttributes.Count == 1)
+			{
+				hasNonNullDataType = hasNonNullDataType || aggregatedDataTypeAttributes[0].DataType != null;
+				continue;
+			}
+
+			int countOfDeclaredNonNullDataTypes = aggregatedDataTypeAttributes.Where(atrMetadata => atrMetadata.DataType != null)
+																			  .Select(atrMetadata => atrMetadata.DataType!)
+																			  .Distinct<ITypeSymbol>(SymbolEqualityComparer.Default)
+																			  .Count();
+			hasNonNullDataType = hasNonNullDataType || countOfDeclaredNonNullDataTypes > 0;
+
+			if (countOfDeclaredNonNullDataTypes > 1)
+			{
+				declaredDataTypeAttributesWithMultipleAggregatedDataTypes ??= new List<DacFieldAttributeInfo>(capacity: 2);
+				declaredDataTypeAttributesWithMultipleAggregatedDataTypes.Add(attributeDeclaredOnProperty);
+			}
+		}
+
+		return new DataTypeAttributesOnDacProperty(allDeclaredDatatypeAttributesOnDacProperty, 
+												   declaredDataTypeAttributesWithMultipleAggregatedDataTypes, hasNonNullDataType);
 	}
 }
