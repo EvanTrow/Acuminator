995906ff5a091cfd355dec8e6230c11950d47f99
Sergey Nikomarov <snikomarov@acumatica.com>
2025-04-06 02:05:08 +0200

    ATR-866: added calculation of data types configured from data type attributes for a DAC field property + the compatibility of these data types with the property type

--- src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/DataTypeAttributesOnDacProperty.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/PXFieldAttributes/Infos/DataTypeAttributesOnDacProperty.cs
@@ -3,6 +3,7 @@ using System.Collections.Immutable;
 using System.Linq;
 
 using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.PXFieldAttributes.Enum;
 using Acuminator.Utilities.Roslyn.Semantic.Attribute;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
 
@@ -12,21 +13,35 @@ namespace Acuminator.Utilities.Roslyn.PXFieldAttributes.Infos;
 
 public sealed class DataTypeAttributesOnDacProperty
 {
+	private readonly DacPropertyInfo _property;
+
 	public ImmutableArray<DacFieldAttributeInfo> AllDeclaredDatatypeAttributesOnDacProperty { get; }
 
 	public ImmutableArray<DacFieldAttributeInfo> DeclaredDataTypeAttributesWithMultipleAggregatedDataTypes { get; }
 
-	public bool HasNonNullDataType { get; }
+	/// <summary>
+	/// Data types configured from data type attributes declared on property including aggregated attributes.
+	/// </summary>
+	public ImmutableArray<ITypeSymbol> DataTypesFromDataTypeAttributes { get; }
 
-	private DataTypeAttributesOnDacProperty(IEnumerable<DacFieldAttributeInfo>? allDeclaredDatatypeAttributesOnDacProperty,
-											IEnumerable<DacFieldAttributeInfo>? declaredDataTypeAttributesWithMultipleAggregatedDataTypes,
-											bool hasNonNullDataType)
+	/// <summary>
+	/// The compatibility of property type and data types from <see cref="DataTypesFromDataTypeAttributes"/> that are configured<br/>
+	/// from data type attributes declared on the property.
+	/// </summary>
+	public CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes PropertyAndDataTypeAttributeTypesCompatibility { get; }
+
+	private DataTypeAttributesOnDacProperty(DacPropertyInfo property, IEnumerable<ITypeSymbol>? dataTypesFromDataTypeAttributes,
+											IEnumerable<DacFieldAttributeInfo>? allDeclaredDatatypeAttributesOnDacProperty,
+											IEnumerable<DacFieldAttributeInfo>? declaredDataTypeAttributesWithMultipleAggregatedDataTypes)
 	{
+		_property = property.CheckIfNull();
+		DataTypesFromDataTypeAttributes = dataTypesFromDataTypeAttributes?.ToImmutableArray() ?? ImmutableArray<ITypeSymbol>.Empty;
 		AllDeclaredDatatypeAttributesOnDacProperty = allDeclaredDatatypeAttributesOnDacProperty?.ToImmutableArray() ?? 
 													 ImmutableArray<DacFieldAttributeInfo>.Empty;
 		DeclaredDataTypeAttributesWithMultipleAggregatedDataTypes = declaredDataTypeAttributesWithMultipleAggregatedDataTypes?.ToImmutableArray() ?? 
 																	ImmutableArray<DacFieldAttributeInfo>.Empty;
-		HasNonNullDataType = hasNonNullDataType;
+
+		PropertyAndDataTypeAttributeTypesCompatibility = GetPropertyAndDataTypeAttributesTypesCompatibility();
 	}
 
 	public static DataTypeAttributesOnDacProperty CollectDataTypeAttributesFromDacProperty(DacPropertyInfo property)
@@ -36,9 +51,9 @@ public sealed class DataTypeAttributesOnDacProperty
 															  .Where(aInfo => !aInfo.AggregatedAttributeMetadata.IsDefaultOrEmpty)
 															  .ToList(capacity: property.Attributes.Length);
 
+		HashSet<ITypeSymbol>? allDataTypesFromDataTypeAttributes = null;
 		List<DacFieldAttributeInfo>? allDeclaredDatatypeAttributesOnDacProperty = null;
 		List<DacFieldAttributeInfo>? declaredDataTypeAttributesWithMultipleAggregatedDataTypes = null;
-		bool hasNonNullDataType = false;
 
 		foreach (var attributeDeclaredOnProperty in declaredAttributesWithFieldTypeMetadata)
 		{
@@ -54,24 +69,59 @@ public sealed class DataTypeAttributesOnDacProperty
 			// HOT PATH optimization 
 			if (aggregatedDataTypeAttributes.Count == 1)
 			{
-				hasNonNullDataType = hasNonNullDataType || aggregatedDataTypeAttributes[0].DataType != null;
+				var singleDataTypeAttribute = aggregatedDataTypeAttributes[0];
+
+				if (singleDataTypeAttribute.DataType != null)
+				{
+					allDataTypesFromDataTypeAttributes ??= new HashSet<ITypeSymbol>(SymbolEqualityComparer.Default);
+					allDataTypesFromDataTypeAttributes.Add(singleDataTypeAttribute.DataType!);
+				}
+
 				continue;
 			}
 
-			int countOfDeclaredNonNullDataTypes = aggregatedDataTypeAttributes.Where(atrMetadata => atrMetadata.DataType != null)
-																			  .Select(atrMetadata => atrMetadata.DataType!)
-																			  .Distinct<ITypeSymbol>(SymbolEqualityComparer.Default)
-																			  .Count();
-			hasNonNullDataType = hasNonNullDataType || countOfDeclaredNonNullDataTypes > 0;
+			var aggregatedDataTypes = aggregatedDataTypeAttributes.Where(atrMetadata => atrMetadata.DataType != null)
+																  .Select(atrMetadata => atrMetadata.DataType!);
+			int countOfNonNullDataTypesAggregatedByAttribute = 0;
 
-			if (countOfDeclaredNonNullDataTypes > 1)
+			foreach (var aggregatedDataType in aggregatedDataTypes)
+			{
+				allDataTypesFromDataTypeAttributes ??= new HashSet<ITypeSymbol>(SymbolEqualityComparer.Default);
+
+				if (allDataTypesFromDataTypeAttributes.Add(aggregatedDataType))
+					countOfNonNullDataTypesAggregatedByAttribute++;
+			}
+
+			if (countOfNonNullDataTypesAggregatedByAttribute > 1)
 			{
 				declaredDataTypeAttributesWithMultipleAggregatedDataTypes ??= new List<DacFieldAttributeInfo>(capacity: 2);
 				declaredDataTypeAttributesWithMultipleAggregatedDataTypes.Add(attributeDeclaredOnProperty);
 			}
 		}
 
-		return new DataTypeAttributesOnDacProperty(allDeclaredDatatypeAttributesOnDacProperty, 
-												   declaredDataTypeAttributesWithMultipleAggregatedDataTypes, hasNonNullDataType);
+		return new DataTypeAttributesOnDacProperty(property, allDataTypesFromDataTypeAttributes, allDeclaredDatatypeAttributesOnDacProperty, 
+												   declaredDataTypeAttributesWithMultipleAggregatedDataTypes);
+	}
+
+	private CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes GetPropertyAndDataTypeAttributesTypesCompatibility()
+	{
+		switch (DataTypesFromDataTypeAttributes.Length)
+		{
+			case 0:
+				//PXDBFieldAttribute and PXEntityAttribute without data type case
+				return CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes.NoDataTypeAttributes;
+
+			case 1:
+				var dataTypeFromAttributes = DataTypesFromDataTypeAttributes[0];
+
+				if (dataTypeFromAttributes.Equals(_property.PropertyTypeUnwrappedNullable, SymbolEqualityComparer.Default))
+					return CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes.CompatibleTypes;
+				else
+					return CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes.IncompatibleTypes;
+
+			default:
+				// Data type attributes configure more than one CLR type for a DAC field
+				return CompatibilityOfDacPropertyTypeAndTypeFromDataTypeAttributes.IncompatibleTypes;
+		}
 	}
 }
