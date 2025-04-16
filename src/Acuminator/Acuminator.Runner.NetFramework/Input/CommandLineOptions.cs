119a22d7fbed668b6d6db60c14ac11256bb20fc9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-04-16 13:25:26 +0200

    ATR-785: reworked command line parameters descriptions with raw string literals

--- src/Acuminator/Acuminator.Runner.NetFramework/Input/CommandLineOptions.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Input/CommandLineOptions.cs
@@ -19,8 +19,10 @@ namespace Acuminator.Runner.Input
 		/// Currently, the supported code sources are C# projects and C# solutions.
 		/// </remarks>
 		[Value(index: 0, MetaName = CommandLineArgNames.CodeSource, Required = true,
-			   HelpText = "A path to the \"code source\" which will be validated. The term \"code source\" is a generalization for components/services that can provide source code to the tool.\n" +
-						  "Currently, the supported code sources are C# projects and C# solutions.")]
+			   HelpText = """
+			A path to the "code source" which will be validated. The term "code source" is a generalization for components/services that can provide source code to the tool.
+			Currently, the supported code sources are C# projects and C# solutions.
+			""")]
 		public string CodeSource { get; }
 
 		/// <summary>
@@ -31,19 +33,27 @@ namespace Acuminator.Runner.Input
 		/// The explicitly specified logger's verbosity.
 		/// </value>
 		[Option(shortName: CommandLineArgNames.VerbosityShort, longName: CommandLineArgNames.VerbosityLong,
-				HelpText = "This optional parameter allows you to explicitly specify logger verbosity. The allowed values are taken from the \"" +
-						  $"{nameof(Serilog)}.{nameof(Serilog.Events)}.{nameof(Serilog.Events.LogEventLevel)} enum.\n\n" +
-						   "The allowed values:\n" +
-						  $"{nameof(LogEventLevel.Verbose)}, {nameof(LogEventLevel.Debug)}, {nameof(LogEventLevel.Information)}, " +
-						  $"{nameof(LogEventLevel.Warning)}, {nameof(LogEventLevel.Error)}, {nameof(LogEventLevel.Fatal)}.\n\n" +
-						  $"By default, the logger will use the \"{nameof(LogEventLevel.Information)}\" verbosity.")]
+				HelpText = "This optional parameter allows you to explicitly specify logger verbosity. The allowed values are taken from the " + 
+						  $"\"{nameof(Serilog)}.{nameof(Serilog.Events)}.{nameof(LogEventLevel)}\" enum.\r\n" +
+						  $"""
+
+						  The allowed values:
+						   - "{nameof(LogEventLevel.Verbose)}", 
+						   - "{nameof(LogEventLevel.Debug)}", 
+						   - "{nameof(LogEventLevel.Information)}",
+						   - "{nameof(LogEventLevel.Warning)}",
+						   - "{nameof(LogEventLevel.Error)}",
+						   - "{nameof(LogEventLevel.Fatal)}".
+
+						  By default, the logger will use the "{nameof(LogEventLevel.Information)}" verbosity."
+						  """)]
 		public string? Verbosity { get; }
 
 		/// <summary>
 		/// If this flag is set to true then the code analysis won't take into consideration suppression comments present in the code.
 		/// </summary>
 		[Option(longName: CommandLineArgNames.DisableSuppressionMechanism,
-				HelpText = "When this optional flag is specified, the code analysis report suppressed Acuminator errors.")]
+				HelpText = "When this optional flag is specified, the code analysis will report Acuminator errors suppressed with Acuminator suppression mechanisms.")]
 		public bool DisableSuppressionMechanism { get; }
 
 		/// <summary>
@@ -53,25 +63,30 @@ namespace Acuminator.Runner.Input
 		/// The optional explicitly specified path to MSBuild.
 		/// </value>
 		[Option(longName: CommandLineArgNames.MSBuildPath,
-				HelpText = "This optional parameter allows you to provide explicitly a path to the MSBuild tool that will be used for analysis.\n" +
-						   "By default, MSBuild installations will be searched automatically on the current machine and the latest found version will be used.")]
+				HelpText = """
+						   This optional parameter allows you to provide explicitly a path to the MSBuild tool that will be used for analysis.
+						   By default, MSBuild installations will be searched automatically on the current machine and the latest found version will be used.
+						   """)]
 		public string? MSBuildPath { get; }
 
 		/// <summary>
-		/// The name of the output file. If not specified then the report will be outputted to the console window.
+		/// The name of the output file. If not specified then the report with analysis results will be outputted to the console window.
 		/// </summary>
 		[Option(shortName: CommandLineArgNames.OutputFileShort, longName: CommandLineArgNames.OutputFileLong,
-				HelpText = "The name of the output file. If not specified then the report will be outputted to the console window.")]
+				HelpText = "The name of the output file. If not specified then the report with analysis results will be outputted to the console window.")]
 		public string? OutputFileName { get; }
 
 		/// <summary>
-		/// This flag regulates how the locations of API usages will be output.<br/>
-		///	By default, file paths in locations are relative to the containing project directory. However, if this flag is set then the absolute file paths will be used.
+		/// This flag regulates how the locations of API usages will be outputted.<br/>
+		///	By default, file paths in locations are relative to the containing project directory.<br/>
+		///	However, if this flag is set, then the absolute file paths will be used.
 		/// </summary>
 		[Option(longName: CommandLineArgNames.OutputAbsolutePathsToUsages,
-				HelpText = "This flag regulates how the locations of API usages will be output.\n" +
-						   "By default, file paths in locations are relative to the containing project directory. " +
-						   "However, if this flag is set then the absolute file paths will be used.\n")]
+				HelpText = """
+						   This flag regulates how the locations of API usages will be outputted.
+						   By default, file paths in locations are relative to the containing project directory.
+						   However, if this flag is set, then the absolute file paths will be used.
+						   """)]
 		public bool OutputAbsolutePathsToUsages { get; }
 
 		/// <summary>
@@ -82,9 +97,11 @@ namespace Acuminator.Runner.Input
 		/// </list>
 		/// </summary>
 		[Option(longName: CommandLineArgNames.OutputFormat,
-				HelpText = "The report output format. There are two supported values:\n" +
-						   "- \"text\" to output the report in plain text, this is the default output mode,\n" +
-						   "- \"json\" to output the report in JSON format.")]
+				HelpText = """
+						   The report output format. There are two supported values:
+						   - "text" to output the report in plain text, this is the default output mode,
+						   - "json" to output the report in JSON format.
+						   """)]
 		public string? OutputFormat { get; }
 
 		// Constructor arguments order must be the same as the properties order. This allows command line parser to initialize immutable options object via constructor.
