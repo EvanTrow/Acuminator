b790795e0a05368c39a7f3884f61c22c5c8f419b
Sergey Nikomarov <snikomarov@acumatica.com>
2025-05-03 23:11:23 +0200

    ATR-785: added initialization of suppression files for all projects in the solution

--- src/Acuminator/Acuminator.Runner.NetFramework/Analysis/Initialization/AcuminatorAnalysisInitializer.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Analysis/Initialization/AcuminatorAnalysisInitializer.cs
@@ -66,11 +66,10 @@ namespace Acuminator.Runner.Analysis.Initialization
 		[SuppressMessage("CodeQuality", "Serilog004:Constant MessageTemplate verifier", Justification = "Ok to use a string variable due to a long message")]
 		public bool InitializeAcuminatorGlobalSuppressionMechanismForProject(Project project)
 		{
-			project.ThrowOnNull();
-			var acuminatorSuppressionFiles = project.AdditionalDocuments
-													.Where(d => IsAcuminatorSuppressionFile(d.FilePath))
-													.Select(d => new SuppressionManagerInitInfo(d.FilePath!, _analysisContext.GenerateSuppressionFile))
-													.ToList(capacity: 1);
+			var acuminatorSuppressionFiles = project.CheckIfNull().AdditionalDocuments
+																  .Where(d => IsAcuminatorSuppressionFile(d.FilePath))
+																  .Select(d => new SuppressionManagerInitInfo(d.FilePath!, _analysisContext.GenerateSuppressionFile))
+																  .ToList(capacity: 1);
 
 			if (_analysisContext.GenerateSuppressionFile && acuminatorSuppressionFiles.Count == 0)
 			{
@@ -92,7 +91,8 @@ namespace Acuminator.Runner.Analysis.Initialization
 					   Acuminator console tool is configured to generate a suppression file for the project "{project.Name}" 
 					   because --{CommandLineArgNames.GenerateSuppressionFileLong} flag was passed among command line arguments.
 					   However, the project does not contain an Acuminator suppression file.
-					   To generate the project's suppression file you need to create an empty suppression file first with the name "{suppressionFileName}" in the project's root folder:
+					   To generate the project's suppression file you need to create an empty suppression file first with the name "{suppressionFileName}" in the project's root folder.
+					   The content of the empty suppression file should be like this:
 
 					   <?xml version="1.0" encoding="utf-8"?>
 					   <suppressions>
@@ -102,6 +102,50 @@ namespace Acuminator.Runner.Analysis.Initialization
 
 					   <ItemGroup>
 					     <AdditionalFiles Include="{suppressionFileName}"/>
+					   </ItemGroup>
+					   """;
+			}
+		}
+
+		[SuppressMessage("CodeQuality", "Serilog004:Constant MessageTemplate verifier", Justification = "Ok to use a string variable due to a long message")]
+		public bool InitializeAcuminatorGlobalSuppressionMechanismForSolution(Solution solution)
+		{
+			var acuminatorSuppressionFiles = solution.CheckIfNull().Projects
+																   .SelectMany(project => project.AdditionalDocuments)
+																   .Where(d => IsAcuminatorSuppressionFile(d.FilePath))
+																   .Select(d => new SuppressionManagerInitInfo(d.FilePath!, _analysisContext.GenerateSuppressionFile))
+																   .ToList(capacity: 1);
+
+			if (_analysisContext.GenerateSuppressionFile && acuminatorSuppressionFiles.Count == 0)
+			{
+				string errorMsg = CreateErrorMessage();
+				_logger.Error(errorMsg);
+				return false;
+			}
+
+			SuppressionManager.InitOrReset(acuminatorSuppressionFiles,
+										   fileSystemServiceFabric: () => new SuppressionFileSystemServiceForConsoleRunner(
+																				new ConsoleRunnerIOErrorObserver(_logger)));
+			return true;
+
+			//-----------------------------------------Local Function-----------------------------------------------------
+			string CreateErrorMessage()
+			{
+				return $"""
+					   Acuminator console tool is configured to generate a suppression file for projects of the solution "{solution.FilePath}" 
+					   because --{CommandLineArgNames.GenerateSuppressionFileLong} flag was passed among command line arguments.
+					   However, the solution does not contain any project with an Acuminator suppression file.
+					   To generate a suppression file for a project you need to create an empty suppression file for the project first with the name "<ProjectName>.{SuppressionFile.SuppressionFileExtension}" in the project's root folder:
+					   The content of the empty suppression file should be like this:
+
+					   <?xml version="1.0" encoding="utf-8"?>
+					   <suppressions>
+					   </suppressions>
+
+					   You must also add it to the "AdditionalFiles" section in the project .csproj file like this:
+
+					   <ItemGroup>
+					     <AdditionalFiles Include="<ProjectName>.{SuppressionFile.SuppressionFileExtension}"/>
 					   </ItemGroup>
 					   """;
 			}
