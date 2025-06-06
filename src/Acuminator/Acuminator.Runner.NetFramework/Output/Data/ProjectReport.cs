0a4fca14f3ed978de0461c3e7fa95c11f2f93db9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-06-06 14:47:30 +0200

    ATR-785: added factory method for successful project reports + adjusted nullable analysis

--- src/Acuminator/Acuminator.Runner.NetFramework/Output/Data/ProjectReport.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Output/Data/ProjectReport.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Linq;
 
-using Acuminator.Utils.Common;
+using Acuminator.Runner.Resources;
+using Acuminator.Utilities.Common;
 
 namespace Acuminator.Runner.Output
 {
@@ -15,13 +18,30 @@ namespace Acuminator.Runner.Output
 
 		public required int DistinctApisCount { get; init; }
 
-		public required ReportGroup ReportDetails { get; init; }
+		public required ReportGroup? ReportDetails { get; init; }
 
         public ProjectReport(string projectName)
         {
-            ProjectName = projectName.ThrowIfNullOrWhiteSpace(nameof(projectName));
+            ProjectName = projectName.CheckIfNullOrWhiteSpace();
         }
 
+		public static ProjectReport SuccessfulReport(string projectName)
+		{
+			var title = string.Format(CultureInfo.CurrentCulture, Messages.AcuminatorValidationPassedMessage, projectName);
+			return new ProjectReport(projectName)
+			{
+				TotalErrorCount   = 0,
+				DistinctApisCount = 0,
+				ReportDetails 	  = new ReportGroup
+				{
+					DistinctApisCount = 0,
+					TotalErrorCount   = 0,
+					GroupTitle 		  = new Title(title, TitleKind.File)
+				}
+			};
+		}
+
+		[MemberNotNullWhen(returnValue: false, nameof(ReportDetails))]
 		public bool IsEmptyReport() => ReportDetails == null ||
 			(ReportDetails.GroupTitle == null &&
 			 ReportDetails.Lines?.Count is null or 0 && ReportDetails.LinesTitle == null &&
