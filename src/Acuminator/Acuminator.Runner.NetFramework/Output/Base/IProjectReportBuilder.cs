5f076eb022e78aa4dda4a7f81be9d238481aa7d9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-06-06 15:14:16 +0200

    ATR-785: updated project report builder interface

--- src/Acuminator/Acuminator.Runner.NetFramework/Output/Base/IProjectReportBuilder.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Output/Base/IProjectReportBuilder.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading;
 
 using Acuminator.Runner.Input;
-using Acuminator.Runner.Output.Data;
 
 using Microsoft.CodeAnalysis;
 
@@ -14,13 +15,23 @@ namespace Acuminator.Runner.Output
 	internal interface IProjectReportBuilder
 	{
 		/// <summary>
-		/// Builds the report from hte diagnostics.
+		/// Builds the report from the diagnostics.
 		/// </summary>
-		/// <param name="diagnosticsWithApis">The diagnostics with banned APIs.</param>
+		/// <param name="foundDiagnostics">Diagnostics reported by Acuminator for the <paramref name="project"/>.</param>
 		/// <param name="analysisContext">The analysis context.</param>
 		/// <param name="project">The project.</param>
 		/// <param name="cancellation">Cancellation token.</param>
-		ProjectReport BuildReport(DiagnosticsWithBannedApis diagnosticsWithApis, AnalysisContext analysisContext, Project project,
+		ProjectReport BuildReport(IEnumerable<Diagnostic> foundDiagnostics, AnalysisContext analysisContext, Project project,
+								  CancellationToken cancellation);
+
+		/// <summary>
+		/// Builds the report from the diagnostics.
+		/// </summary>
+		/// <param name="foundDiagnostics">Diagnostics reported by Acuminator for the <paramref name="project"/>.</param>
+		/// <param name="analysisContext">The analysis context.</param>
+		/// <param name="project">The project.</param>
+		/// <param name="cancellation">Cancellation token.</param>
+		ProjectReport BuildReport(ImmutableArray<Diagnostic> foundDiagnostics, AnalysisContext analysisContext, Project project,
 								  CancellationToken cancellation);
 	}
 }
