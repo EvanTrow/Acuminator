3c706ebb4a07646469c5f8b466d655cf931d3d1a
Sergey Nikomarov <snikomarov@acumatica.com>
2025-06-09 02:42:43 +0200

    ATR-785: refactoring of the grouper interface - moved to sub namespace, renamed members, changed comments

--- src/Acuminator/Acuminator.Runner.NetFramework/Output/Grouping/Base/IGroupLines.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Output/Grouping/Base/IGroupLines.cs
@@ -7,7 +7,7 @@ using Acuminator.Runner.Input;
 
 using Microsoft.CodeAnalysis;
 
-namespace Acuminator.Runner.Output
+namespace Acuminator.Runner.Output.Grouping
 {
 	/// <summary>
 	/// Interface for helpers that group report lines.
@@ -15,21 +15,21 @@ namespace Acuminator.Runner.Output
 	internal interface IGroupLines
 	{
 		/// <summary>
-		/// Gets the required output results grouping.
+		/// The grouping mode of the grouper.
 		/// </summary>
 		GroupingMode Grouping { get; }
 
 		/// <summary>
-		/// Get API groups
+		/// Get Acuminator errors grouped according to the <see cref="Grouping"/> mode.
 		/// </summary>
 		/// <param name="analysisContext">Analysis context.</param>
 		/// <param name="diagnostics">Diagnostics to group.</param>
 		/// <param name="projectDirectory">Pathname of the project directory.</param>
 		/// <param name="cancellation">Cancellation token.</param>
 		/// <returns>
-		/// Output API results grouped by <see cref="Grouping"/>.
+		/// Acuminator errors grouped according to the <see cref="Grouping"/> mode.
 		/// </returns>
-		IEnumerable<ReportGroup> GetApiGroups(AnalysisContext analysisContext, ImmutableArray<Diagnostic> diagnostics,
-											  string? projectDirectory, CancellationToken cancellation);
+		IEnumerable<ReportGroup> GetGroupedErrors(AnalysisContext analysisContext, ImmutableArray<Diagnostic> diagnostics,
+												  string? projectDirectory, CancellationToken cancellation);
 	}
 }
