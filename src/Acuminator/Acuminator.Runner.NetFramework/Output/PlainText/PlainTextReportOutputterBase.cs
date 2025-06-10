39f3b37d4ec6e0f5a44192e4529407db119038cc
Sergey Nikomarov <snikomarov@acumatica.com>
2025-06-10 02:19:01 +0200

    ATR-785: adjusted plain text base report outputter to Acuminator

--- src/Acuminator/Acuminator.Runner.NetFramework/Output/PlainText/PlainTextReportOutputterBase.cs
+++ src/Acuminator/Acuminator.Runner.NetFramework/Output/PlainText/PlainTextReportOutputterBase.cs
@@ -5,7 +5,7 @@ using System.Threading;
 
 using Acuminator.Runner.Input;
 using Acuminator.Runner.Output.Data;
-using Acuminator.Utils.Common;
+using Acuminator.Utilities.Common;
 
 using Serilog;
 
@@ -18,21 +18,15 @@ namespace Acuminator.Runner.Output.PlainText
 	{
 		public abstract void Dispose();
 
-		public virtual void OutputReport(CodeSourceReport codeSourceReport, AppAnalysisContext analysisContext, CancellationToken cancellation)
+		public virtual void OutputReport(CodeSourceReport codeSourceReport, AnalysisContext analysisContext, CancellationToken cancellation)
 		{
-			codeSourceReport.ThrowIfNull(nameof(codeSourceReport));
+			codeSourceReport.ThrowOnNull();
 			cancellation.ThrowIfCancellationRequested();
 
 			WriteLine($"{codeSourceReport.CodeSourceName} - Total Errors Count: {codeSourceReport.TotalErrorCount}");
 
-			if (!analysisContext.IncludeAllDistinctApis || codeSourceReport.DistinctApis?.Count is null or 0)
-				WriteLine($"{codeSourceReport.CodeSourceName} - Distinct APIs Count: {codeSourceReport.DistinctApisCount}");
-
 			if (codeSourceReport.TotalErrorCount == 0)
 				return;
-
-			if (analysisContext.IncludeAllDistinctApis && codeSourceReport.DistinctApis?.Count > 0)
-				OutputDistinctApis(codeSourceReport.DistinctApis);
 
 			foreach (ProjectReport projectReport in codeSourceReport.ProjectReports)
 			{
@@ -43,35 +37,24 @@ namespace Acuminator.Runner.Output.PlainText
 			}
 		}
 
-		private void OutputDistinctApis(IReadOnlyCollection<Line> distinctApis)
+		public virtual void OutputReport(ProjectReport projectReport, AnalysisContext analysisContext, CancellationToken cancellation)
 		{
-			WriteLine();
-			WriteDistinctApisTitle("Distinct APIs", depth: 0, distinctApis.Count);
-
-			foreach (Line api in distinctApis)
-				WriteLine(api, depth: 1);
-
-			WriteLine();
-		}
-
-		public virtual void OutputReport(ProjectReport projectReport, AppAnalysisContext analysisContext, CancellationToken cancellation)
-		{
-			projectReport.ThrowIfNull(nameof(projectReport));
+			projectReport.ThrowOnNull();
 			cancellation.ThrowIfCancellationRequested();
 
-			WriteLine($"{projectReport.ProjectName} - Total Errors Count: {projectReport.TotalErrorCount}");
-			WriteLine($"{projectReport.ProjectName} - Distinct APIs Count: {projectReport.DistinctApisCount}");
+			WriteLine($"{projectReport.ProjectName} - Total Errors Count: {projectReport.TotalDiagnosticsCount}");
+			WriteLine($"{projectReport.ProjectName} - Distinct Diagnostics Count: {projectReport.DistinctDiagnosticsCount}");
 
-			if (projectReport.TotalErrorCount == 0)
+			if (projectReport.TotalDiagnosticsCount == 0)
 				return;
 			
 			if (projectReport.ReportDetails != null)
 			{
-				OutputApiGroup(projectReport.ReportDetails, depth: 0, cancellation, recursionDepth: 0);
+				OutputReportGroup(projectReport.ReportDetails, groupIndentationLevel: 0, recursionDepth: 0, cancellation);
 			}
 		}
 
-		protected virtual void OutputApiGroup(ReportGroup reportGroup, int depth, CancellationToken cancellation, int recursionDepth)
+		protected virtual void OutputReportGroup(ReportGroup reportGroup, int groupIndentationLevel, int recursionDepth, CancellationToken cancellation)
 		{
 			cancellation.ThrowIfCancellationRequested();
 
@@ -83,66 +66,87 @@ namespace Acuminator.Runner.Output.PlainText
 				return;
 			}
 
-			bool hasTitle = false, hasLines = false, hasSubGroups = false;
-			
-			if (reportGroup.GroupTitle.HasValue)
+			bool hasTitle = reportGroup.GroupTitle.HasValue;
+
+			if (hasTitle)
 			{
-				hasTitle = true;
-				WriteTitle(reportGroup.GroupTitle.Value, depth, reportGroup.TotalErrorCount, reportGroup.DistinctApisCount, reportGroup.HasContent);
+				WriteTitle(reportGroup.GroupTitle!.Value, groupIndentationLevel, reportGroup.TotalDiagnosticsCount,
+						   reportGroup.DistinctDiagnosticsCount, reportGroup.HasContent);
 			}
 
 			cancellation.ThrowIfCancellationRequested();
-
-			if (reportGroup.Lines?.Count > 0)
-			{
-				hasLines = true;
-				int linesDepth;
-
-				if (reportGroup.LinesTitle.HasValue)
-				{
-					WriteTitle(reportGroup.LinesTitle.Value, depth + 1, reportGroup.Lines.Count, reportGroup.DistinctApisCount, reportGroup.HasContent);
-					linesDepth = depth + 2;
-				}
-				else
-					linesDepth = depth + 1;
-
-				foreach (Line line in reportGroup.Lines)
-				{
-					cancellation.ThrowIfCancellationRequested();
-					WriteLine(line, linesDepth);
-				}
-			}
+			bool hasLines = OutputReportGroupLines(reportGroup, groupIndentationLevel, cancellation);
 
 			cancellation.ThrowIfCancellationRequested();
-
-			if (reportGroup.ChildrenGroups?.Count > 0)
-			{
-				hasSubGroups = true;
-				int groupDepth;
-
-				if (reportGroup.ChildrenTitle.HasValue)
-				{
-					int totalSubGroupErrors = reportGroup.ChildrenGroups.Sum(group => group.TotalErrorCount);
-					WriteTitle(reportGroup.ChildrenTitle.Value, depth + 1, totalSubGroupErrors, reportGroup.DistinctApisCount, reportGroup.HasContent);
-					groupDepth = depth + 2;
-				}
-				else
-					groupDepth = depth + 1;
-
-				foreach (ReportGroup childGroup in reportGroup.ChildrenGroups)
-				{
-					cancellation.ThrowIfCancellationRequested();
-					OutputApiGroup(childGroup, groupDepth, cancellation, recursionDepth + 1);
-				}
-			}
+			bool hasSubGroups = OutputReportGroupSubgroups(reportGroup, groupIndentationLevel, recursionDepth, cancellation);
 
 			if (!hasSubGroups && (hasLines || hasTitle))
 				WriteLine();
 		}
 
-		protected abstract void WriteDistinctApisTitle(string titleText, int depth, int itemsCount);
+		/// <summary>
+		/// Output report group lines.
+		/// </summary>
+		/// <param name="reportGroup">The report group to use to output lines.</param>
+		/// <param name="groupIndentationLevel">The indentation level of <paramref name="reportGroup"/>.</param>
+		/// <param name="cancellation">Cancellation.</param>
+		/// <returns>
+		/// True if <paramref name="reportGroup"/> has lines, false if not.
+		/// </returns>
+		private bool OutputReportGroupLines(ReportGroup reportGroup, int groupIndentationLevel, CancellationToken cancellation)
+		{
+			if (reportGroup.Lines?.Count is null or 0)
+				return false;
 
-		protected abstract void WriteTitle(in Title? title, int depth, int itemsCount, int distinctApisCount, bool hasContent);
+			int linesIndentationLevel;
+
+			if (reportGroup.LinesTitle.HasValue)
+			{
+				WriteTitle(reportGroup.LinesTitle.Value, indentationLevel: groupIndentationLevel + 1, reportGroup.Lines.Count, 
+						   reportGroup.DistinctDiagnosticsCount, reportGroup.HasContent);
+				linesIndentationLevel = groupIndentationLevel + 2;
+			}
+			else
+				linesIndentationLevel = groupIndentationLevel + 1;
+
+			foreach (Line line in reportGroup.Lines)
+			{
+				cancellation.ThrowIfCancellationRequested();
+				WriteLine(line, linesIndentationLevel);
+			}
+
+			return true;
+		}
+
+		private bool OutputReportGroupSubgroups(ReportGroup reportGroup, int groupIndentationLevel, int recursionDepth, CancellationToken cancellation)
+		{
+			if (reportGroup.ChildrenGroups?.Count is null or 0)
+				return false;
+
+			int subgroupIndentationLevel;
+
+			if (reportGroup.ChildrenTitle.HasValue)
+			{
+				int totalSubGroupDiagnostics = reportGroup.ChildrenGroups.Sum(group => group.TotalDiagnosticsCount);
+
+				WriteTitle(reportGroup.ChildrenTitle.Value, indentationLevel: groupIndentationLevel + 1, totalSubGroupDiagnostics,
+						   reportGroup.DistinctDiagnosticsCount, reportGroup.HasContent);
+				
+				subgroupIndentationLevel = groupIndentationLevel + 2;
+			}
+			else
+				subgroupIndentationLevel = groupIndentationLevel + 1;
+
+			foreach (ReportGroup childGroup in reportGroup.ChildrenGroups)
+			{
+				cancellation.ThrowIfCancellationRequested();
+				OutputReportGroup(childGroup, subgroupIndentationLevel, recursionDepth + 1, cancellation);
+			}
+
+			return true;
+		}
+
+		protected abstract void WriteTitle(in Title? title, int indentationLevel, int diagnosticsCount, int? distinctDiagnosticsCount, bool hasContent);
 
 		protected void WriteLine<T>(T obj)
 		{
