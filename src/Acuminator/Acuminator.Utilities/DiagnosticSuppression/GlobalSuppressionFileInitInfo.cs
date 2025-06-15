0355b62d6bbf533f50fc1b52ff162ec61d0223c7
Sergey Nikomarov <snikomarov@acumatica.com>
2025-06-15 19:34:50 +0200

    ATR-785: renamed SuppressionManagerInitInfo into GlobalSuppressionFileInitInfo and integrated GlobalSuppressionWorkMode into it

---
+++ src/Acuminator/Acuminator.Utilities/DiagnosticSuppression/GlobalSuppressionFileInitInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Utilities.DiagnosticSuppression
+{
+	public readonly struct GlobalSuppressionFileInitInfo : IEquatable<GlobalSuppressionFileInitInfo>
+	{
+		public string Path { get; }
+
+		public GlobalSuppressionWorkMode SuppressionWorkMode { get; }
+
+		public GlobalSuppressionFileInitInfo(string path, GlobalSuppressionWorkMode suppressionWorkMode)
+		{
+			Path = path.CheckIfNullOrWhiteSpace();
+			SuppressionWorkMode = suppressionWorkMode;
+		}
+
+		public override bool Equals(object obj) => 
+			obj is GlobalSuppressionFileInitInfo initInfo && Equals(initInfo);
+
+		public bool Equals(GlobalSuppressionFileInitInfo other) => 
+			SuppressionWorkMode == other.SuppressionWorkMode && 
+			string.Equals(Path, other.Path, StringComparison.OrdinalIgnoreCase);
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+
+			unchecked
+			{
+				hash = 23 * hash + (Path?.ToUpperInvariant().GetHashCode() ?? 0);
+				hash = 23 * hash + SuppressionWorkMode.GetHashCode();
+			}
+
+			return hash;
+		}
+	}
+}
