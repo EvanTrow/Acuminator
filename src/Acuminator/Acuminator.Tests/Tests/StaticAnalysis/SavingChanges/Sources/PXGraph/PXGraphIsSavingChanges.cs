c060d7138e9aff1dcd88c45ae21248e769ab1ba7
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-07 02:30:38 +0200

    ATR-727: updated tests for PX1058 with cases for Configure and Initialize methods

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/SavingChanges/Sources/PXGraph/PXGraphIsSavingChanges.cs
@@ -0,0 +1,31 @@
+﻿using PX.Data;
+using PX.Data.WorkflowAPI;
+using PX.SM;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.SavingChanges.Sources
+{
+	public class SMUserMaint : PXGraph<SMUserMaint>, PX.Data.DependencyInjection.IGraphWithInitialization
+	{
+		public PXSelect<Users> Users;
+
+		public SMUserMaint()
+		{
+			int icount = Users.Select().Count;
+
+			if (icount > 1)
+			{
+				Users.Delete(Users.Current);
+				Actions.PressSave();
+			}
+		}
+
+		public void Initialize() =>
+			Actions.PressSave();
+
+		public override void Configure(PXScreenConfiguration graph)
+		{
+			base.Configure(graph);
+			Actions.PressSave();
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/SavingChanges/Sources/PXGraph/PXGraphNotSavingChanges.cs
@@ -0,0 +1,22 @@
+﻿using PX.Data;
+using PX.Data.WorkflowAPI;
+using PX.SM;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.SavingChanges.Sources
+{
+	public class SMUserMaint : PXGraph<SMUserMaint>, PX.Data.DependencyInjection.IGraphWithInitialization
+	{
+		private readonly int _icount;
+
+		public PXSelect<Users> Users;
+
+		public SMUserMaint()
+		{
+			_icount = Users.Select().Count;
+		}
+
+		public void Initialize() { }
+
+		public override void Configure(PXScreenConfiguration graph) => base.Configure(graph);
+	}
+}
