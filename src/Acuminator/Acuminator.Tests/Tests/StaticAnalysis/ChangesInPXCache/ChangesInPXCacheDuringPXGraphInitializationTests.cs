0502e82506175ccf051e2320fc70e6ecba5b013d
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-07 16:21:52 +0200

    ATR-727: updated PX1059 unit tests with checks for Initialize, Configure, and constructors in graph extensions

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ChangesInPXCache/ChangesInPXCacheDuringPXGraphInitializationTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ChangesInPXCache/ChangesInPXCacheDuringPXGraphInitializationTests.cs
@@ -4,57 +4,96 @@ using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.ChangesInPXCache
 {
-    public class ChangesInPXCacheDuringPXGraphInitializationTests : DiagnosticVerifier
-    {
-        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
-            new PXGraphAnalyzer(
-                CodeAnalysisSettings.Default
-                .WithRecursiveAnalysisEnabled(),
-                new ChangesInPXCacheDuringPXGraphInitializationAnalyzer());
+	public class ChangesInPXCacheDuringPXGraphInitializationTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new PXGraphAnalyzer(
+				CodeAnalysisSettings.Default
+									.WithRecursiveAnalysisEnabled()
+									.WithStaticAnalysisEnabled(),
+				new ChangesInPXCacheDuringPXGraphInitializationAnalyzer());
 
-        [Theory]
-        [EmbeddedFileData(@"PXGraph\PXGraphChangesPXCacheInInstanceConstructor.cs")]
-        public void GraphInstanceConstructor_ReportsDiagnostic(string source)
-        {
-            VerifyCSharpDiagnostic(source,
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(16, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(17, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(18, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(20, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(21, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(22, 17));
-        }
+		[Theory]
+		[EmbeddedFileData(@"PXGraph\PXGraphChangesPXCache.cs")]
+		public void Graph_Initialization_ChangesInPXCache(string source)
+		{
+			VerifyCSharpDiagnostic(source,
+				// Constructor
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(16, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(17, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(18, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(20, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(21, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(22, 5),
 
-        [Theory]
-        [EmbeddedFileData(@"PXGraph\PXGraphChangesPXCacheInInstanceConstructorViaMethod.cs")]
-        public void GraphInstanceConstructorViaMethod_ReportsDiagnostic(string source)
-        {
-            VerifyCSharpDiagnostic(source, Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(16, 17));
-        }
+				// Initialize
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(32, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(33, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(34, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(36, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(37, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(38, 5),
 
-        [Theory]
-        [EmbeddedFileData(@"PXGraph\PXGraphExtensionChangesPXCacheInInitializationMethod.cs")]
-        public void GraphExtensionInitializationMethod_ReportsDiagnostic(string source)
-        {
-            VerifyCSharpDiagnostic(source,
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(14, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(15, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(16, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(18, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(19, 17),
-                Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(20, 17));
-        }
+				// Configure
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(50, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(51, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(52, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(54, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(55, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(56, 5)
+			);
+		}
 
-        [Theory]
-        [EmbeddedFileData(@"PXGraph\PXGraphDoesntChangePXCacheInInstanceConstructor.cs")]
-        public void GraphInstanceConstructor_DoesntReportsDiagnostic(string source)
-        {
-            VerifyCSharpDiagnostic(source);
-        }
-    }
+		[Theory]
+		[EmbeddedFileData(@"PXGraph\PXGraphChangesPXCacheViaMethod.cs")]
+		public void Graph_Initialization_ChangesInPXCache_ViaCalledMethod(string source)
+		{
+			VerifyCSharpDiagnostic(source,
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(17, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(21, 31),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(26, 4));
+		}
+
+		[Theory]
+		[EmbeddedFileData(@"PXGraph\PXGraphExtensionChangesPXCache.cs")]
+		public void GraphExtension_Initialization_ChangesInPXCache(string source)
+		{
+			VerifyCSharpDiagnostic(source,
+				// Constructor
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(16, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(17, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(18, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(20, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(21, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(22, 5),
+
+				// Initialize
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(32, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(33, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(34, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(36, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(37, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(38, 5),
+
+				// Configure
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(50, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(51, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(52, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(54, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(55, 5),
+				Descriptors.PX1059_ChangesInPXCacheDuringPXGraphInitialization.CreateFor(56, 5));
+		}
+
+		[Theory]
+		[EmbeddedFileData(@"PXGraph\PXGraphDoesntChangePXCache.cs")]
+		public void Graph_Initialization_DoesNotChangePXCache_DoesntReportsDiagnostic(string source) => 
+			VerifyCSharpDiagnostic(source);
+	}
 }
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ChangesInPXCache/Sources/PXGraph/PXGraphDoesntChangePXCache.cs
@@ -0,0 +1,27 @@
+﻿using PX.Data;
+using PX.Data.WorkflowAPI;
+using PX.SM;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.ChangesInPXCache.Sources.PXGraph
+{
+	public class SMUserMaint : PXGraph<SMUserMaint>, PX.Data.DependencyInjection.IGraphWithInitialization
+	{
+		public PXSelect<Users> Users;
+
+		public SMUserMaint()
+		{
+			int icount = Users.Select().Count;
+		}
+
+		public void Initialize()
+		{
+			int icount = Users.Select().Count;
+		}
+
+		public override void Configure(PXScreenConfiguration graph)
+		{
+			base.Configure(graph);
+			int icount = Users.Select().Count;
+		}
+	}
+}
