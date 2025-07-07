da69dba90c6f10886c3d71c7378e92fe84bae1ea
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-07 15:46:17 +0200

    ATR-727: updated PX1081 unit tests with checks for Initialize, Configure, and constructors in graph extensions

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXActionExecution/Sources/PXGraph/PressWithAdapterInGraphExtension.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXActionExecution/Sources/PXGraph/PressWithAdapterInGraphExtension.cs
@@ -1,24 +1,36 @@
 ﻿using PX.Data;
+using PX.Data.WorkflowAPI;
 
-namespace PX.Objects
+namespace Acuminator.Tests.Tests.StaticAnalysis.PXActionExecution
 {
-    public class SOInvoiceEntryExt : PXGraphExtension<SOInvoiceEntry>
-    {
-        public override void Initialize()
-        {
-            Base.Release.Press(null);
-        }
-    }
+	public class SOInvoiceEntryExt : PXGraphExtension<SOInvoiceEntry>
+	{
+		public SOInvoiceEntryExt()
+		{
+			Base.Release.Press(null);
+		}
 
-    public class SOInvoiceEntry : PXGraph<SOInvoiceEntry, SOInvoice>
-    {
-        public PXAction<SOInvoice> Release;
-    }
+		public override void Initialize() =>
+			Base.Release.Press(null);
 
-    public class SOInvoice : IBqlTable
-    {
-        [PXDBString(8, IsKey = true, InputMask = "")]
-        public string RefNbr { get; set; }
-        public abstract class refNbr : IBqlField { }
-    }
+		public override void Configure(PXScreenConfiguration configuration)
+		{
+			base.Configure(configuration);
+			Base.Release.Press(null);
+		}
+	}
+
+	public class SOInvoiceEntry : PXGraph<SOInvoiceEntry, SOInvoice>
+	{
+		public PXAction<SOInvoice> Release;
+
+		public PXSelect<SOInvoice> MainView;
+	}
+
+	public class SOInvoice : IBqlTable
+	{
+		[PXDBString(8, IsKey = true, InputMask = "")]
+		public string RefNbr { get; set; }
+		public abstract class refNbr : IBqlField { }
+	}
 }
