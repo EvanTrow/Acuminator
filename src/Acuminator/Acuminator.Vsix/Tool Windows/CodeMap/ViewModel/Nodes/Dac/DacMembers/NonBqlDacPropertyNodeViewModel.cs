a22f152f8b11ace009f571a0f21d301ca751b4e3
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-09 23:16:53 +0200

    ATR-851: added tree node view model for non-BQL DAC property

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacMembers/NonBqlDacPropertyNodeViewModel.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Vsix.ToolWindows.Common;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class NonBqlDacPropertyNodeViewModel : DacMemberNodeViewModel, IElementWithTooltip
+	{
+		public override Icon NodeIcon => Icon.DacNonBqlProperty;
+
+		public DacPropertyInfo PropertyInfo => (MemberInfo as DacPropertyInfo)!;
+
+		public NonBqlDacPropertyNodeViewModel(DacMemberCategoryNodeViewModel dacMemberCategoryVM, TreeNodeViewModel parent, 
+											  DacPropertyInfo nonBqlPropertyInfo, bool isExpanded = false) :
+										 base(dacMemberCategoryVM, parent, nonBqlPropertyInfo, isExpanded)
+		{
+		}
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => 
+			treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+
+		TooltipInfo? IElementWithTooltip.CalculateTooltip()
+		{
+			string? tooltip = PropertyInfo.Node?.ToString();
+			return tooltip.IsNullOrWhiteSpace()
+				? null
+				: new TooltipInfo(tooltip) { TrimExcess = true };
+		}
+	}
+}
