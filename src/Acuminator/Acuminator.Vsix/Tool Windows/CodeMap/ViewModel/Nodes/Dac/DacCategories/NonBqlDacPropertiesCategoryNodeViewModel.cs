b8c783bdee1f257606b5fd1c0b6d60ea3ab23644
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-09 22:29:48 +0200

    ATR-851: added non BQL DAC properties category view model for node

---
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/DacCategories/NonBqlDacPropertiesCategoryNodeViewModel.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Vsix.ToolWindows.CodeMap.Dac;
+
+namespace Acuminator.Vsix.ToolWindows.CodeMap
+{
+	public class NonBqlDacPropertiesCategoryNodeViewModel : DacFieldCategoryNodeViewModel
+	{
+		protected override bool AllowNavigation => true;
+
+		public override Icon NodeIcon => Icon.DacNonBqlPropertiesCategory;
+
+		public NonBqlDacPropertiesCategoryNodeViewModel(DacNodeViewModel dacViewModel, TreeNodeViewModel parent, bool isExpanded) : 
+												   base(dacViewModel, parent, DacMemberCategory.NonBqlProperties, isExpanded)
+		{
+		}
+
+		public override IEnumerable<DacFieldInfo> GetCategoryDacFields() => 
+			DacModel.DeclaredDacFields.Where(field => field.IsNonBqlProperty);
+
+		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) => treeVisitor.VisitNode(this, input);
+
+		public override TResult AcceptVisitor<TResult>(CodeMapTreeVisitor<TResult> treeVisitor) => treeVisitor.VisitNode(this);
+
+		public override void AcceptVisitor(CodeMapTreeVisitor treeVisitor) => treeVisitor.VisitNode(this);
+	}
+}
