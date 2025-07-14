0ac05bf6f72568f5c8900ab63028067233d939d7
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-14 02:33:10 +0200

    ATR-892: added unit test to check graph semantic model creation for circular references

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Graph/CircularGraphExtensionReferencesTests.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Graph
+{
+	public class CircularGraphExtensionReferencesTests : SemanticModelTestsBase<PXGraphSemanticModel>
+	{
+		[Theory]
+		[EmbeddedFileData("GraphExtensionWIthCircularReference.cs")]
+		public async Task SecondLevel_Derived_GraphExtension_InfoCollection(string text)
+		{
+			var graphSemanticModel = await PrepareSemanticModelAsync(text).ConfigureAwait(false);
+			graphSemanticModel.Should().BeNull();
+		}
+
+#pragma warning disable CS8609 // Nullability of reference types in return type doesn't match overridden member.
+		protected override Task<PXGraphSemanticModel?> PrepareSemanticModelAsync(RoslynTestContext context, CancellationToken cancellation)
+		{
+			var graphOrGraphExtDeclaration = context.Root.DescendantNodes()
+														 .OfType<ClassDeclarationSyntax>()
+														 .FirstOrDefault();
+			graphOrGraphExtDeclaration.Should().NotBeNull();
+
+			INamedTypeSymbol? graphOrGraphExtSymbol = context.SemanticModel.GetDeclaredSymbol(graphOrGraphExtDeclaration);
+			graphOrGraphExtSymbol.Should().NotBeNull();
+
+			var graphSemanticModel = PXGraphSemanticModel.InferModel(context.PXContext, graphOrGraphExtSymbol!,
+																	 GraphSemanticModelCreationOptions.CollectGeneralGraphInfo,
+																	 cancellation: cancellation);
+			return Task.FromResult(graphSemanticModel);
+		}
+#pragma warning restore CS8609 // Nullability of reference types in return type doesn't match overridden member.
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Graph/Sources/GraphExtensionWIthCircularReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PX.Data;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Graph.Sources
+{
+	public class Extension1 : PXGraphExtension<Extension2, MyGraph> { }
+
+	public class Extension2 : PXGraphExtension<Extension1, MyGraph> { }
+
+	public class MyGraph : PXGraph<MyGraph>
+	{ }
+}
