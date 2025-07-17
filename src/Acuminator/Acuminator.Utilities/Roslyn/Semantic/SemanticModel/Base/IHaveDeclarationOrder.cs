191b3f82d3717ad6f5106642f91e1f79272652af
Sergey Nikomarov <snikomarov@acumatica.com>
2025-07-17 15:11:19 +0200

    ATR-892: refactored out DeclarationOrder into a separate interface to better follow the interface segregation principle

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Base/IHaveDeclarationOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace Acuminator.Utilities.Roslyn.Semantic;
+
+/// <summary>
+/// An interface for a semantic info object that has a declaration order.
+/// </summary>
+public interface IHaveDeclarationOrder
+{
+	/// <summary>
+	/// Gets the declaration order.
+	/// </summary>
+	/// <value>
+	/// The declaration order.
+	/// </value>
+	int DeclarationOrder { get; }
+}
--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Base/IOverridableItem.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Base/IOverridableItem.cs
@@ -1,39 +1,32 @@
-﻿#nullable enable
+﻿namespace Acuminator.Utilities.Roslyn.Semantic;
 
-using Acuminator.Utilities.Common;
-
-namespace Acuminator.Utilities.Roslyn.Semantic
+/// <summary>
+/// An interface for a DTO which stores info about some item. The item which can be overridable, and the info about base item is also stored.
+/// </summary>
+/// <typeparam name="T">Generic type parameter.</typeparam>
+public interface IOverridableItem<out T>
+where T : IOverridableItem<T>
 {
+	string Name { get; }
+
 	/// <summary>
-	/// An interface for a DTO which stores info about some item. The item which can be overridable, and the info about base item is also stored.
+	/// The overriden base info if any.
 	/// </summary>
-	/// <typeparam name="T">Generic type parameter.</typeparam>
-	public interface IOverridableItem<out T>
-	where T : IOverridableItem<T>
+	T? Base { get; }
+}
+
+internal interface IWriteableBaseItem<T> : IOverridableItem<T>
+where T : IOverridableItem<T>
+{
+	/// <inheritdoc cref="IOverridableItem{T}.Base"/>
+	new T? Base
 	{
-		string Name { get; }
-
-		/// <summary>
-		/// The overriden base info if any.
-		/// </summary>
-		T? Base { get; }
-
-		int DeclarationOrder { get; }
+		get;
+		set;
 	}
 
-	internal interface IWriteableBaseItem<T> : IOverridableItem<T>
-	where T : IOverridableItem<T>
-	{
-		/// <inheritdoc cref="IOverridableItem{T}.Base"/>
-		new T? Base
-		{
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Combine this info with info from base types.
-		/// </summary>
-		void CombineWithBaseInfo(T baseInfo);
-	}
+	/// <summary>
+	/// Combine this info with info from base types.
+	/// </summary>
+	void CombineWithBaseInfo(T baseInfo);
 }
