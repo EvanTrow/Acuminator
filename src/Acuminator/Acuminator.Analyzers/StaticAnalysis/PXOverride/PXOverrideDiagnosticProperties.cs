244dbc8d1137ad4cd485206e70048e5f44321ea8
Sergey Nikomarov <snikomarov@acumatica.com>
2025-08-03 00:53:04 +0200

    ATR-739: added MemberVirtualityKind and diagnostic properties for PX1097 code fix

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXOverride/PXOverrideDiagnosticProperties.cs
@@ -0,0 +1,15 @@
+﻿namespace Acuminator.Analyzers.StaticAnalysis.PXOverride
+{
+	internal static class PXOverrideDiagnosticProperties
+	{
+		/// <summary>
+		/// Indicates whether the patch method is a non-public method.
+		/// </summary>
+		public const string IsNonPublicPatchMethod = nameof(IsNonPublicPatchMethod);
+
+		/// <summary>
+		/// Property stores the <see cref="MemberVirtualityKind"/> value for a patch method.
+		/// </summary>
+		public const string PatchMethodVirtualityKind = nameof(PatchMethodVirtualityKind);
+	}
+}
