f5aace7119dc3ccfe7a7b31a95c511e1b1f5366d
Sergey Nikomarov <snikomarov@acumatica.com>
2025-08-03 18:41:49 +0200

    ATR-739: added unit tests for PX1097 and its code fix for main scenario

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/PublicNonVirtualPXOverrideTests.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Analyzers.StaticAnalysis.PXOverride;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.PXOverride
+{
+	public class PublicNonVirtualPXOverrideTests : CodeFixVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new PXGraphAnalyzer(
+				CodeAnalysisSettings.Default
+									.WithRecursiveAnalysisEnabled()
+									.WithStaticAnalysisEnabled()
+									.WithSuppressionMechanismDisabled(),
+				new PXOverrideAnalyzer());
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new NonPublicOrVirtualPXOverrideFix();
+
+		[Theory]
+		[EmbeddedFileData(@"PublicNonVirtual\ExtensionWithNonPublicVirtualPXOverride.cs")]
+		public Task NonPublic_And_Virtual_PXOverrides(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1097_PXOverrideMethodMustBePublicNonVirtual.CreateFor(11, 25, "TestMethod1"),
+				Descriptors.PX1097_PXOverrideMethodMustBePublicNonVirtual.CreateFor(17, 27, "TestMethod2"),
+				Descriptors.PX1097_PXOverrideMethodMustBePublicNonVirtual.CreateFor(23, 35, "TestMethod3"),
+				Descriptors.PX1097_PXOverrideMethodMustBePublicNonVirtual.CreateFor(29, 8, "TestMethod4"));
+
+		[Theory]
+		[EmbeddedFileData(@"PublicNonVirtual\ExtensionWithNonPublicVirtualPXOverride_Expected.cs")]
+		public Task NonPublic_And_Virtual_PXOverrides_AfterCodeFix(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData(@"PublicNonVirtual\ExtensionWithNonPublicVirtualPXOverride.cs", 
+						  @"PublicNonVirtual\ExtensionWithNonPublicVirtualPXOverride_Expected.cs")]
+		public Task NonPublic_And_Virtual_PXOverrides_CodeFix(string actual, string expected) => 
+			VerifyCSharpFixAsync(actual, expected);
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/PublicNonVirtual/ExtensionWithNonPublicVirtualPXOverride_Expected.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using PX.Data;
+
+namespace Acuminator.Tests.Sources
+{
+
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class DerivedExtension : PXGraphExtension<BaseExtension, MyGraph>
+	{
+		[PXOverride]
+		public object TestMethod1(int x, bool drilldown, double y, Func<int, bool, double, object> del)
+		{
+			return del(x, drilldown, y);
+		}
+
+		[PXOverride]
+		public void TestMethod2(int x, Action<int> del)
+		{
+			del(x);
+		}
+
+		[PXOverride]
+		public void TestMethod3(Action del)
+		{
+			del();
+		}
+
+		[PXOverride]
+		public void TestMethod4(Action del)
+		{
+			del();
+		}
+	}
+
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class BaseExtension : PXGraphExtension<MyGraph>
+	{
+		public virtual object TestMethod1(int x, bool drilldown, double y)
+		{
+			return new object();
+		}
+	}
+
+
+	public class MyGraph : PXGraph<MyGraph>
+	{
+		public virtual void TestMethod2(int x)
+		{	
+		}
+
+		public virtual void TestMethod3()
+		{
+		}
+
+		public virtual void TestMethod4()
+		{
+		}
+	}
+}
