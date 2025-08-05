0527a51b185dd6b0e3d92175952e563addebc42a
Sergey Nikomarov <snikomarov@acumatica.com>
2025-08-06 01:26:02 +0200

    ATR-888: added unit tests for PX1079 diagnostic and code fix

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/PXOverrideWithoutBaseDelegateParameterTests.cs
@@ -0,0 +1,54 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Analyzers.StaticAnalysis.PXOverride;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.PXOverride
+{
+	public class PXOverrideWithoutBaseDelegateParameterTests : CodeFixVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new PXGraphAnalyzer(
+				CodeAnalysisSettings.Default
+									.WithRecursiveAnalysisEnabled()
+									.WithStaticAnalysisEnabled()
+									.WithSuppressionMechanismDisabled(),
+				new PXOverrideAnalyzer());
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new AddBaseDelegateParameterFix();
+
+		[Theory]
+		[EmbeddedFileData(@"HasBaseDelegateParameter\PXOverrideWithoutBaseDelegateParameter.cs")]
+		public Task PXOverrides_Without_BaseDelegate_Parameter(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1079_PXOverrideWithoutDelegateParameter.CreateFor(12, 16, "TestMethod1"),
+				Descriptors.PX1079_PXOverrideWithoutDelegateParameter.CreateFor(15, 15, "TestMethod2"),
+				Descriptors.PX1079_PXOverrideWithoutDelegateParameter.CreateFor(20, 17, "TestMethod3"),
+				Descriptors.PX1079_PXOverrideWithoutDelegateParameter.CreateFor(26, 15, "TestMethod4"));
+
+		[Theory]
+		[EmbeddedFileData(@"HasBaseDelegateParameter\PXOverrideWithCustomBaseDelegateParameter.cs")]
+		public Task PXOverrides_With_BaseDelegateParameter_WithCustomDelegateTypes(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData(@"HasBaseDelegateParameter\PXOverrideWithoutBaseDelegateParameter_Expected.cs")]
+		public Task PXOverrides_Without_BaseDelegate_Parameter_AfterCodeFix(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData(@"HasBaseDelegateParameter\PXOverrideWithoutBaseDelegateParameter.cs",
+						  @"HasBaseDelegateParameter\PXOverrideWithoutBaseDelegateParameter_Expected.cs")]
+		public Task PXOverrides_Without_BaseDelegate_Parameter_CodeFix(string actual, string expected) => 
+			VerifyCSharpFixAsync(actual, expected);
+	}
+}
