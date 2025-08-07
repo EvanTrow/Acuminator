5a0accc7a09e238a0c8b5a923f62dca13d24326d
Sergey Nikomarov <snikomarov@acumatica.com>
2025-08-07 04:03:19 +0200

    ATR-900: added unit tests for the PX1100 diagnostic

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ObsoleteElementsUsage/ObsoleteElementsUsageTests.cs
@@ -0,0 +1,38 @@
+﻿#nullable enable
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.ObsoleteElementsUsage;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.ObsoleteElementsUsage
+{
+	public class ObsoleteElementsUsageTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new PXGraphAnalyzer(
+				CodeAnalysisSettings.Default
+									.WithRecursiveAnalysisEnabled()
+									.WithStaticAnalysisEnabled()
+									.WithSuppressionMechanismDisabled(),
+				new ObsoleteElementsUsageAnalyzer());
+
+		[Theory]
+		[EmbeddedFileData(@"PXOverride\PXOverrideOfObsoleteMethods.cs")]
+		public Task PXOverrides_With_Incorrect_BaseDelegate_Parameter(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1100_PXOverrideOverridesObsoleteMethod.CreateFor(12, 17),
+				Descriptors.PX1100_PXOverrideOverridesObsoleteMethod.CreateFor(18, 15),
+				Descriptors.PX1100_PXOverrideOverridesObsoleteMethod.CreateFor(23, 15),
+				Descriptors.PX1100_PXOverrideOverridesObsoleteMethod.CreateFor(29, 15),
+				Descriptors.PX1100_PXOverrideOverridesObsoleteMethod.CreateFor(39, 17));
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ObsoleteElementsUsage/Sources/PXOverride/PXOverrideOfObsoleteMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PX.Data;
+
+namespace Acuminator.Tests.Sources
+{
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class DerivedExtension : PXGraphExtension<BaseExtension, MyGraph>
+	{
+		[PXOverride]
+		public object TestMethod1(int x, bool drilldown, double y, Func<int, bool, double, object> del)
+		{
+			return del(x, drilldown, y);
+		}
+
+		[PXOverride]
+		public void TestMethod2(int x)
+		{
+		}
+
+		[PXOverride]
+		public void TestMethod3(IEnumerable<string> source, Action<IEnumerable<string>> del)
+		{
+			del(source);
+		}
+
+		[PXOverride]
+		public void TestMethod4(Action del)
+		{
+			del();
+		}
+	}
+
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class BaseExtension : PXGraphExtension<MyGraph>
+	{
+		[PXOverride]
+		public object TestMethod1(int x, bool drilldown, double y)
+		{
+			return new object();
+		}
+	}
+
+
+	public class MyGraph : PXGraph<MyGraph>
+	{
+		[Obsolete]
+		public virtual object TestMethod1(int x, bool drilldown, double y)
+		{
+			return new object();
+		}
+
+		[Obsolete("Obsolete")]
+		public virtual void TestMethod2(int x)
+		{	
+		}
+
+		[Obsolete("Obsolete", error: true)]
+		public virtual void TestMethod3(IEnumerable<string> source)
+		{
+		}
+
+		[Obsolete]
+		public virtual void TestMethod4()
+		{
+		}
+	}
+}
