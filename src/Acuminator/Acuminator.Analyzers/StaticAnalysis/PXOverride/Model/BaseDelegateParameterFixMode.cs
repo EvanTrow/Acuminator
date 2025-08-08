55427731325dd05cdc25040f20abb5a3c206dce0
Sergey Nikomarov <snikomarov@acumatica.com>
2025-08-09 00:57:47 +0200

    ATR-896: added BaseDelegateParameterFixMode enum mode for PXOverride code fixes

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXOverride/Model/BaseDelegateParameterFixMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Acuminator.Analyzers.StaticAnalysis.PXOverride
+{
+	/// <summary>
+	/// Values that represent different modes for code fix of the base delegate parameter.
+	/// </summary>
+	public enum BaseDelegateParameterFixMode : byte
+	{
+		/// <summary>
+		/// Code fix shoud add a base delegate parameter to the method.
+		/// </summary>
+		AddDelegateParameter,
+
+		/// <summary>
+		/// Code fix shoud replace a base delegate parameter of the method with a generated correct delegate parameter.
+		/// </summary>
+		ReplaceDelegateParameter,
+
+		/// <summary>
+		/// Code fix shoud rename a base delegate parameter of the method with a correct name.
+		/// </summary>
+		RenameDelegateParameter
+	}
+}
