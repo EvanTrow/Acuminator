6cd992e343d4301ff87eff252ec5286c0b3b5ca5
Sergey Nikomarov <snikomarov@acumatica.com>
2025-09-16 02:22:12 +0200

    ATR-507: added DAC field kind enum + minor optimizations

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Enums/DacFieldKind.cs
@@ -0,0 +1,28 @@
+﻿namespace Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+/// <summary>
+/// Values that represent DAC field kinds.
+/// </summary>
+public enum DacFieldKind : byte
+{
+	Regular,
+
+	NoteID,
+	Tstamp,
+	GroupMask,
+	Attributes,
+
+	CreatedByID,
+	CreatedByScreenID,
+	CreatedDateTime	,
+	LastModifiedByID,
+	LastModifiedByScreenID,
+	LastModifiedDateTime,
+
+	DeletedDatabaseRecord,
+	DatabaseRecordStatus,
+	CompanyID,
+	CompanyMask,
+	Notes,
+	Files
+}
