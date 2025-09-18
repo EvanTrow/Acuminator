562f3e201a2686137cc8dbe90a0367b012a96592
Sergey Nikomarov <snikomarov@acumatica.com>
2025-09-18 18:35:24 +0200

    ATR-507: added container for PX1069 diagnostic properties passed to the code fix

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/MissingMandatoryDacFields/PX1069Properties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+namespace Acuminator.Analyzers.StaticAnalysis.MissingMandatoryDacFields;
+
+internal static class PX1069Properties
+{
+	/// <summary>
+	/// Property stores comma-separated <see cref="DacFieldKind"/> values that represent mandatory DAC fields missing in the DAC.
+	/// </summary>
+	public const string MissingMandatoryDacFields = nameof(MissingMandatoryDacFields);
+}
