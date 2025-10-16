8a59474015f277c388a7f213cd4ddf6d83e3af63
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-16 21:04:39 +0200

    ATR-507: added another example of DAC with missing mandatory fields to the demo solution

---
+++ src/Samples/PX.Objects.HackathonDemo/PX.Objects.HackathonDemo/DAC/MissingMandatoryFieldsInDac/DacWithFieldsAddedToBeginning.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+using System;
+
+using PX.Data;
+using PX.Data.BQL;
+
+namespace Acuminator.Tests.Sources
+{
+	/// <exclude/>
+	[PXCacheName("DAC with fields added to beginning")]
+	public class DacWithFieldsAddedToBeginning : IBqlTable
+	{
+		#region CreatedDateTime
+		public abstract class createdDateTime : BqlDateTime.Field<createdDateTime> { }
+
+		[PXDBCreatedDateTime]
+		public virtual DateTime? CreatedDateTime { get; set; }
+		#endregion
+		#region LastModifiedByID
+		public abstract class lastModifiedByID : BqlGuid.Field<lastModifiedByID> { }
+
+		[PXDBLastModifiedByID]
+		public virtual Guid? LastModifiedByID { get; set; }
+		#endregion
+		#region LastModifiedByScreenID
+		public abstract class lastModifiedByScreenID : BqlString.Field<lastModifiedByScreenID> { }
+
+		[PXDBLastModifiedByScreenID]
+		public virtual string? LastModifiedByScreenID { get; set; }
+		#endregion
+		#region LastModifiedDateTime
+		public abstract class lastModifiedDateTime : BqlDateTime.Field<lastModifiedDateTime> { }
+
+		[PXDBLastModifiedDateTime]
+		public virtual DateTime? LastModifiedDateTime { get; set; }
+		#endregion
+
+		#region ID
+		[PXDBIdentity(IsKey = true)]
+		public virtual int? ID { get; set; }
+		public abstract class iD : PX.Data.BQL.BqlInt.Field<iD> { }
+		#endregion
+
+		#region Description
+		[PXDBString(255)]
+		public virtual string? Description { get; set; }
+		public abstract class description : PX.Data.BQL.BqlString.Field<description> { }
+		#endregion
+
+		#region Tstamp
+		public abstract class Tstamp : BqlByteArray.Field<Tstamp> { }
+
+		[PXDBTimestamp]
+		public virtual byte[]? tstamp { get; set; }
+		#endregion
+	}
+}
