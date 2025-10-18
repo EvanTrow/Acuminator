c48695858097e524cd231694c54d7ab64879e583
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-18 03:08:36 +0200

    ATR-343: added DTOs for missing DAC field infos

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/MissingMandatoryDacFields/MissingDacFieldInfos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+
+namespace Acuminator.Analyzers.StaticAnalysis.MissingMandatoryDacFields;
+
+public readonly record struct MissingMandatoryDacFieldInfo(DacFieldKind FieldKind, DacFieldInsertMode InsertMode);
+
+public readonly record struct MissingDacFieldsInfos(List<MissingMandatoryDacFieldInfo> MissingAuditAndTimestampInfos,
+													MissingMandatoryDacFieldInfo? MissingNoteIdFieldInfo);
