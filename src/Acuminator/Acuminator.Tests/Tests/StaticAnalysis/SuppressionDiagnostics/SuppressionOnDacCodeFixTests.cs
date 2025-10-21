7d804e4976f6ff172793b89f01e66c5e03001cb8
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-21 05:36:01 +0200

    ATR-693: added unit tests for generation of suppression comment in attribute lists

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/SuppressionDiagnostics/SuppressionOnDacCodeFixTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/SuppressionDiagnostics/SuppressionOnDacCodeFixTests.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.ConstructorInDac;
 using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Analyzers.StaticAnalysis.DacExtensionDefaultAttribute;
 using Acuminator.Analyzers.StaticAnalysis.ForbiddenFieldsInDac;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
@@ -27,13 +28,19 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.SuppressionDiagnostics
 									.WithSuppressionMechanismEnabled(),
 
 				new ForbiddenFieldsInDacAnalyzer(),
-				new ConstructorInDacAnalyzer());
+				new ConstructorInDacAnalyzer(),
+				new DacExtensionDefaultAttributeAnalyzer());
 
 		protected override CodeFixProvider GetCSharpCodeFixProvider() => new SuppressDiagnosticTestCodeFix();
 
 		[Theory]
 		[EmbeddedFileData(@"Dac\ForbiddenFieldsSuppressComment_Expected.cs")]
-		public virtual Task DacForbiddenFields_Suppressed(string source) =>
+		public virtual Task DacForbiddenFields_Suppressed_NoDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData(@"Dac\DacExtension_SuppressionInAttributeLists_Expected.cs")]
+		public virtual Task DacExtension_Alert_InAttributes_Suppressed_NoDiagnostic(string source) =>
 			VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
@@ -44,15 +51,19 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.SuppressionDiagnostics
 				Descriptors.PX1028_ConstructorInDacDeclaration.CreateFor(line: 95, column: 10));
 
 		[Theory]
-		[EmbeddedFileData(@"Dac\ForbiddenFields.cs",
-			@"Dac\ForbiddenFieldsSuppressComment_Expected.cs")]
+		[EmbeddedFileData(@"Dac\ForbiddenFields.cs", @"Dac\ForbiddenFieldsSuppressComment_Expected.cs")]
 		public virtual Task DacWithForbidden_SuppressComment_CodeFix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 
 		[Theory]
-		[EmbeddedFileData(@"Dac\WithConstructor.cs",
-			@"Dac\WithConstructorSuppressComment_Expected.cs")]
+		[EmbeddedFileData(@"Dac\WithConstructor.cs", @"Dac\WithConstructorSuppressComment_Expected.cs")]
 		public virtual Task DacWithConstructor_SuppressComment_CodeFix(string actual, string expected) =>
+			VerifyCSharpFixAsync(actual, expected);
+
+		[Theory]
+		[EmbeddedFileData(@"Dac\DacExtension_SuppressionInAttributeLists.cs", 
+						  @"Dac\DacExtension_SuppressionInAttributeLists_Expected.cs")]
+		public virtual Task DacExtension_Alert_InAttributes_SuppressComment_CodeFix(string actual, string expected) =>
 			VerifyCSharpFixAsync(actual, expected);
 	}
 }
