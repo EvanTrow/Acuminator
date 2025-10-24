c757b09afc9f51b6d2af38fd660041d6e2df68ed
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-24 04:54:59 +0200

    ATR-914: added display of PXAccumulator DAC indicator to Code Map

--- src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/Dac/BaseDacPlaceholderNodeViewModel.cs
+++ src/Acuminator/Acuminator.Vsix/Tool Windows/CodeMap/ViewModel/Nodes/Dac/Dac/BaseDacPlaceholderNodeViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
 using Acuminator.Vsix.ToolWindows.CodeMap.Dac;
 using Acuminator.Vsix.ToolWindows.Common;
@@ -43,10 +44,37 @@ namespace Acuminator.Vsix.ToolWindows.CodeMap
 			DacOrDacExtInfo	  = dacOrDacExtInfo.CheckIfNull();
 			ContainingDacNode = containingDacNode;
 
-			var dacTypeInfo = CreateDacTypeInfo();
-			ExtraInfos		= new ExtendedObservableCollection<ExtraInfoViewModel>(dacTypeInfo);
+			var extraDacInfos = CreateExtraInfos();
+			ExtraInfos		  = new ExtendedObservableCollection<ExtraInfoViewModel>(extraDacInfos);
 
 			_tooltipLazy = new Lazy<TooltipInfo?>(CalculateTooltipFromAttributes);
+		}
+
+		private IEnumerable<ExtraInfoViewModel> CreateExtraInfos()
+		{
+			yield return CreateDacTypeInfo();
+
+			if (HasPXAccumulatorAttribute())
+			{
+				yield return new IconViewModel(this, Icon.PXAccumulatorDac);
+			}
+		}
+
+		private bool HasPXAccumulatorAttribute()
+		{
+			var attributes = DacOrDacExtInfo.Symbol.GetAttributes();
+
+			if (attributes.IsDefaultOrEmpty)
+				return false;
+
+			var pxContext = ContainingDacNode.DacModel.PXContext;
+			var pxAccumulatorAttribute = pxContext.AttributeTypes.PXAccumulatorAttribute;
+
+			if (pxAccumulatorAttribute == null)
+				return false;
+
+			bool hasPXAccumulator = attributes.Any(attr => attr.AttributeClass?.InheritsFromOrEquals(pxAccumulatorAttribute) == true);
+			return hasPXAccumulator;
 		}
 
 		public override TResult AcceptVisitor<TInput, TResult>(CodeMapTreeVisitor<TInput, TResult> treeVisitor, TInput input) =>
