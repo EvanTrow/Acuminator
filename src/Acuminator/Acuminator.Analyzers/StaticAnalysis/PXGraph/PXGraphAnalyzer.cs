45d4fa0d1f7b2778e137b0661c8ef6f07a4be764
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-25 11:42:55 +0200

    ATR-728: implemented analyzer for the PX1112 analyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/PXGraphAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/PXGraph/PXGraphAnalyzer.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +10,7 @@ using Acuminator.Analyzers.StaticAnalysis.CallingBaseDataViewDelegate;
 using Acuminator.Analyzers.StaticAnalysis.ChangesInPXCache;
 using Acuminator.Analyzers.StaticAnalysis.ConstructorInGraphExtension;
 using Acuminator.Analyzers.StaticAnalysis.DatabaseQueries;
+using Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisGraphAndDac;
 using Acuminator.Analyzers.StaticAnalysis.ForbidPrivateEventHandlers;
 using Acuminator.Analyzers.StaticAnalysis.InvalidPXActionSignature;
 using Acuminator.Analyzers.StaticAnalysis.LongOperationStart;
@@ -45,6 +45,7 @@ namespace Acuminator.Analyzers.StaticAnalysis.PXGraph
 		protected override SymbolKind SymbolKind => SymbolKind.NamedType;
 
 		public PXGraphAnalyzer() : this(null,
+			new GraphAndGraphExtensionDeclarationAnalyzer(),
 			new PXGraphCreationInGraphInWrongPlacesGraphAnalyzer(),
 			new ConstructorInGraphExtensionAnalyzer(),
 			new SavingChangesInGraphSemanticModelAnalyzer(),
