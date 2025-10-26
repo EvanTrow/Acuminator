bc738c9dceb4cc2048e455b30ef7e9bf1acbefa9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-26 15:21:12 +0100

    ATR-777: added unit tests for PX1113 diagnostic and code fix

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DeclarationAnalysisGraphAndDac/Sources/SealedGraphsAndGraphExtensions/SealedGraphExtension_Expected.cs
@@ -0,0 +1,18 @@
+using PX.Data;
+
+namespace PX.Objects
+{
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class SealedGraphExtension : PXGraphExtension<MyGraph>
+	{
+	}
+
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public partial class SealedPartialGraphExtension : PXGraphExtension<MyGraph> { }
+
+	public partial class SealedPartialGraphExtension : PXGraphExtension<MyGraph> { }
+
+	public class MyGraph : PXGraph<MyGraph>
+	{
+	}
+}
