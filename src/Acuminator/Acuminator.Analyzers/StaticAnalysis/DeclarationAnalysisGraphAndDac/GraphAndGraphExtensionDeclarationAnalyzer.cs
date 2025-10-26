ce349b31d4109781f3455b7bdeed0c57995f4bb2
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-26 20:41:16 +0100

    ATR-778: implemented the PX1114 analyzer

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DeclarationAnalysisGraphAndDac/GraphAndGraphExtensionDeclarationAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DeclarationAnalysisGraphAndDac/GraphAndGraphExtensionDeclarationAnalyzer.cs
@@ -9,6 +9,7 @@ using Acuminator.Utilities.Common;
 using Acuminator.Utilities.DiagnosticSuppression;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Semantic.PXGraph;
+using Acuminator.Utilities.Roslyn.Syntax.PXGraph;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -23,7 +24,8 @@ namespace Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisGraphAndDac
 			ImmutableArray.Create
 			(
 				Descriptors.PX1112_GenericGraphsAndGraphExtensionsMustBeAbstract,
-				Descriptors.PX1113_SealedGraphsAndGraphExtensions
+				Descriptors.PX1113_SealedGraphsAndGraphExtensions,
+				Descriptors.PX1114_GraphExtensionInheritFromNonAbstractGraphExtension
 			);
 
 		public override void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphEventSemanticModel graphOrGraphExt)
@@ -33,6 +35,12 @@ namespace Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisGraphAndDac
 
 			context.CancellationToken.ThrowIfCancellationRequested();
 			CheckIfGraphOrGraphExtensionIsSealed(context, pxContext, graphOrGraphExt);
+
+			if (graphOrGraphExt.GraphType == GraphType.PXGraphExtension)
+			{
+				context.CancellationToken.ThrowIfCancellationRequested();
+				CheckIfGraphExtensionInheritsFromNonAbstractGraphExtension(context, pxContext, graphOrGraphExt);
+			}
 		}
 
 		protected virtual void CheckIfGraphOrGraphExtensionIsGenericNonAbstract(SymbolAnalysisContext context, PXContext pxContext,
@@ -69,6 +77,55 @@ namespace Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisGraphAndDac
 											Descriptors.PX1113_SealedGraphsAndGraphExtensions, location);
 
 			context.ReportDiagnosticWithSuppressionCheck(diagnostic, pxContext.CodeAnalysisSettings);
+		}
+
+		protected virtual void CheckIfGraphExtensionInheritsFromNonAbstractGraphExtension(SymbolAnalysisContext context, PXContext pxContext,
+																						  PXGraphEventSemanticModel graphExtension)
+		{
+			var pxProtectedAccessAttribute = pxContext.AttributeTypes.PXProtectedAccessAttribute;
+			bool isDerivedFromTerminalGraphExtension = 
+				graphExtension.Symbol.GetGraphExtensionBaseTypes()
+									 .OfType<INamedTypeSymbol>()
+									 .Any(baseExtension => IsTerminalGraphExtension(baseExtension, pxProtectedAccessAttribute));
+
+			if (!isDerivedFromTerminalGraphExtension)
+				return;
+
+			var location = GetDiagnosticLocation();
+			var diagnostic = Diagnostic.Create(
+											Descriptors.PX1114_GraphExtensionInheritFromNonAbstractGraphExtension, location);
+			context.ReportDiagnosticWithSuppressionCheck(diagnostic, pxContext.CodeAnalysisSettings);
+
+			//-----------------------------------------------Local Function------------------------------------------------
+			Location? GetDiagnosticLocation()
+			{
+				var semanticModel = context.Compilation.GetSemanticModel(graphExtension.Node!.SyntaxTree);
+
+				if (semanticModel == null)
+				{
+					return graphExtension.Node.Identifier.GetLocation().NullIfLocationKindIsNone() ??
+						   graphExtension.Node.GetLocation();
+				}
+
+				var baseGraphInfo = GraphSyntaxUtils.GetBaseGraphTypeInfo(semanticModel, pxContext, graphExtension.Node,
+																		  context.CancellationToken);
+				var location = baseGraphInfo?.TypeNode.GetLocation().NullIfLocationKindIsNone();
+				location ??= graphExtension.Node.Identifier.GetLocation().NullIfLocationKindIsNone() ??
+							 graphExtension.Node.GetLocation();
+				return location;
+			}
+		}
+
+		private static bool IsTerminalGraphExtension(INamedTypeSymbol graphExtension, INamedTypeSymbol? pxProtectedAccessAttribute)
+		{
+			if (!graphExtension.TypeParameters.IsDefaultOrEmpty)
+				return false;
+			else if (!graphExtension.IsAbstract)
+				return true;
+
+			return pxProtectedAccessAttribute != null 
+				? graphExtension.HasAttribute(pxProtectedAccessAttribute, checkOverrides: false)
+				: false;
 		}
 	}
 }
