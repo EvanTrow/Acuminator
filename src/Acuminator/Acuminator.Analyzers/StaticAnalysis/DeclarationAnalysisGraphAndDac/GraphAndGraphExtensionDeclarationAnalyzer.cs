a0a2ee12a1702924c5da353a3a4572e944a07f27
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-26 03:09:37 +0100

    ATR-777:  implemented PX1113 diagnostic

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DeclarationAnalysisGraphAndDac/GraphAndGraphExtensionDeclarationAnalyzer.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/DeclarationAnalysisGraphAndDac/GraphAndGraphExtensionDeclarationAnalyzer.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading;
 
 using Acuminator.Analyzers.StaticAnalysis.PXGraph;
 using Acuminator.Utilities.Common;
@@ -22,29 +22,50 @@ namespace Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisGraphAndDac
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
 			ImmutableArray.Create
 			(
-				Descriptors.PX1112_GenericGraphsAndGraphExtensionsMustBeAbstract
+				Descriptors.PX1112_GenericGraphsAndGraphExtensionsMustBeAbstract,
+				Descriptors.PX1113_SealedGraphsAndGraphExtensions
 			);
 
 		public override void Analyze(SymbolAnalysisContext context, PXContext pxContext, PXGraphEventSemanticModel graphOrGraphExt)
 		{
 			context.CancellationToken.ThrowIfCancellationRequested();
-
 			CheckIfGraphOrGraphExtensionIsGenericNonAbstract(context, pxContext, graphOrGraphExt);
+
+			context.CancellationToken.ThrowIfCancellationRequested();
+			CheckIfGraphOrGraphExtensionIsSealed(context, pxContext, graphOrGraphExt);
 		}
 
 		protected virtual void CheckIfGraphOrGraphExtensionIsGenericNonAbstract(SymbolAnalysisContext context, PXContext pxContext,
 																				PXGraphEventSemanticModel graphOrGraphExt)
 		{
 			if (graphOrGraphExt.Symbol.IsAbstract || !graphOrGraphExt.Symbol.IsGenericType ||
-				graphOrGraphExt.Symbol.TypeParameters.IsDefaultOrEmpty || graphOrGraphExt.Node == null)
+				graphOrGraphExt.Symbol.TypeParameters.IsDefaultOrEmpty)
 			{
 				return;
 			}
 			
-			var location = graphOrGraphExt.Node.Identifier.GetLocation().NullIfLocationKindIsNone() ??
+			var location = graphOrGraphExt.Node!.Identifier.GetLocation().NullIfLocationKindIsNone() ??
 						   graphOrGraphExt.Node.GetLocation();
 			var diagnostic = Diagnostic.Create(Descriptors.PX1112_GenericGraphsAndGraphExtensionsMustBeAbstract, location,
 												graphOrGraphExt.Symbol.Name);
+
+			context.ReportDiagnosticWithSuppressionCheck(diagnostic, pxContext.CodeAnalysisSettings);
+		}
+
+		protected virtual void CheckIfGraphOrGraphExtensionIsSealed(SymbolAnalysisContext context, PXContext pxContext,
+																	PXGraphEventSemanticModel graphOrGraphExt)
+		{
+			if (!graphOrGraphExt.Symbol.IsSealed)
+				return;
+
+			var sealedToken = graphOrGraphExt.Node!.Modifiers.FirstOrDefault(m => m.IsKind(SyntaxKind.SealedKeyword));
+			var location = sealedToken != default
+				? sealedToken.GetLocation().NullIfLocationKindIsNone()
+				: null;
+
+			location ??= graphOrGraphExt.Node.Identifier.GetLocation().NullIfLocationKindIsNone() ??
+						 graphOrGraphExt.Node.GetLocation();
+			var diagnostic = Diagnostic.Create(Descriptors.PX1113_SealedGraphsAndGraphExtensions, location, graphOrGraphExt.Symbol.Name);
 
 			context.ReportDiagnosticWithSuppressionCheck(diagnostic, pxContext.CodeAnalysisSettings);
 		}
