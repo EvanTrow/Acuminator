288ecffc9bdc49a1ac2e3446ecf54500f468a8b6
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-30 00:17:53 +0100

    ATR-762: refactoring - removed redundant code

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Utils/GraphSymbolUtils.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Utils/GraphSymbolUtils.cs
@@ -1,17 +1,12 @@
-﻿#nullable enable
-
-using System;
+﻿using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading;
 
 using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn.Constants;
 using Acuminator.Utilities.Roslyn.Semantic.Dac;
-using Acuminator.Utilities.Roslyn.Semantic.AcumaticaEvents;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 {
