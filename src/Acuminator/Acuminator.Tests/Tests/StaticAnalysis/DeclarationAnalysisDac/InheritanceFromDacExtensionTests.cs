710bde6d7982f9569a0b69ea36138e62e4ada250
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-29 19:56:57 +0100

    ATR-762: reworked PX1009 tests

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DeclarationAnalysisDac/InheritanceFromDacExtensionTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DeclarationAnalysisDac/InheritanceFromDacExtensionTests.cs
@@ -1,54 +1,52 @@
-﻿using Acuminator.Analyzers.StaticAnalysis;
+﻿using System;
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
 using Acuminator.Analyzers.StaticAnalysis.Dac;
 using Acuminator.Analyzers.StaticAnalysis.DeclarationAnalysisDac;
 using Acuminator.Tests.Helpers;
 using Acuminator.Tests.Verification;
 using Acuminator.Utilities;
+
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
+
 using Xunit;
 
 namespace Acuminator.Tests.Tests.StaticAnalysis.DeclarationAnalysisDac
 {
-    public class InheritanceFromDacExtensionTests : CodeFixVerifier
-    {
-	    protected override CodeFixProvider GetCSharpCodeFixProvider() => new InheritanceFromDacExtensionFix();
-	    
-	    protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+	public class InheritanceFromDacExtensionTests : CodeFixVerifier
+	{
+		protected override CodeFixProvider GetCSharpCodeFixProvider() => new InheritanceFromDacExtensionFix();
+		
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
 			new DacAnalyzersAggregator(
 				CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
 											.WithSuppressionMechanismDisabled(),
 				new DacAndDacExtensionDeclarationAnalyzer());
 
 		[Theory]
-        [EmbeddedFileData("InheritanceFromPXCacheExtension_Good.cs")]
-        public void TestDiagnostic_ShouldNotShowDiagnostic(string actual)
-        {
-            VerifyCSharpDiagnostic(actual);
-        }
+		[EmbeddedFileData(@"InheritanceFromDacExtension\InheritanceFromDacExtension_Good.cs")]
+		public Task Sealed_DacExtension_WithCorrectBaseType_NoDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
-		[EmbeddedFileData("InheritanceFromPXCacheExtension_Bad.cs")]
-		public void TestDiagnostic(string actual)
-        {
-            VerifyCSharpDiagnostic(actual,
-	            Descriptors.PX1009_InheritanceFromDacExtension.CreateFor(10, 15),
-	            Descriptors.PX1011_NotSealedDacExtension.CreateFor(12, 15),
-	            Descriptors.PX1011_NotSealedDacExtension.CreateFor(13, 15));
-        }
-
-	    [Theory]
-	    [EmbeddedFileData("InheritanceFromPXMappedCacheExtension.cs")]
-	    public void TestDiagnostic_PXMappedCacheExtension_ShouldNotShowDiagnostic(string actual)
-	    {
-		    VerifyCSharpDiagnostic(actual);
-	    }
+		[EmbeddedFileData(@"InheritanceFromDacExtension\InheritanceFromDacExtension.cs")]
+		public Task DacExtension_NotSealed_And_DerivedFromAnotherDacExtension(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
+				Descriptors.PX1009_InheritanceFromDacExtension.CreateFor(10, 15),
+				Descriptors.PX1011_NotSealedDacExtension.CreateFor(12, 15),
+				Descriptors.PX1011_NotSealedDacExtension.CreateFor(13, 15));
 
 		[Theory]
-	    [EmbeddedFileData("InheritanceFromPXCacheExtension_Bad.cs", "InheritanceFromPXCacheExtension_Bad_Expected.cs")]
-		public void TestCodeFix(string actual, string expected)
-	    {
-		    VerifyCSharpFix(actual, expected);
-	    }
-    }
+		[EmbeddedFileData(@"InheritanceFromDacExtension\InheritanceFromMappedDacExtension.cs")]
+		public Task MappedDacExtension_NoDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
+
+		[Theory]
+		[EmbeddedFileData(@"InheritanceFromDacExtension\InheritanceFromDacExtension.cs",
+						  @"InheritanceFromDacExtension\InheritanceFromDacExtension_Expected.cs")]
+		public Task DacExtension_DerivedFromAnotherDacExtension_CodeFix_ChangeBaseType(string source, string expected) =>
+			VerifyCSharpFixAsync(source, expected);
+	}
 }
