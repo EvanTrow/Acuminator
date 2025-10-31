5c4e3b76a4006c285b6f09754c3ccf492cbbf4de
Sergey Nikomarov <snikomarov@acumatica.com>
2025-10-31 13:38:52 +0100

    ATR-743: refactoring - fixed indentation and sorted usings

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Symbols/PXReferentialIntegritySymbols.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/Symbols/PXReferentialIntegritySymbols.cs
@@ -1,24 +1,26 @@
 ﻿using System;
-using System.Collections.Immutable;
-using Microsoft.CodeAnalysis;
-using Acuminator.Utilities.Roslyn.Constants;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+
+using Acuminator.Utilities.Roslyn.Constants;
+
+using Microsoft.CodeAnalysis;
 
 namespace Acuminator.Utilities.Roslyn.Semantic.Symbols
 {
-    /// <summary>
-    /// A referential integrity symbols related to Acumatica PK/FK API.
-    /// </summary>
-    public class PXReferentialIntegritySymbols : SymbolsSetBase
-    {
-        public static ImmutableHashSet<string> ForeignKeyContainerNames { get; } =
-            new HashSet<string>
-            {
-                TypeNames.ReferentialIntegrity.AsSimpleKeyName,
-                TypeNames.ReferentialIntegrity.ForeignKeyOfName,
-                TypeNames.ReferentialIntegrity.CompositeKey
-            }
-            .ToImmutableHashSet();
+	/// <summary>
+	/// A referential integrity symbols related to Acumatica PK/FK API.
+	/// </summary>
+	public class PXReferentialIntegritySymbols : SymbolsSetBase
+	{
+		public static ImmutableHashSet<string> ForeignKeyContainerNames { get; } =
+			new HashSet<string>
+			{
+				TypeNames.ReferentialIntegrity.AsSimpleKeyName,
+				TypeNames.ReferentialIntegrity.ForeignKeyOfName,
+				TypeNames.ReferentialIntegrity.CompositeKey
+			}
+			.ToImmutableHashSet();
 
 		/// <summary>
 		/// The maximum size of the DAC primary key.
@@ -28,63 +30,63 @@ namespace Acuminator.Utilities.Roslyn.Semantic.Symbols
 		/// </remarks>
 		public const int MaxPrimaryKeySize = 12;
 
-        /// <summary>
-        /// Gets the primary key interface.
-        /// </summary>
-        /// <value>
-        /// The primary key interface.
-        /// </value>
-        public INamedTypeSymbol IPrimaryKey { get; }
+		/// <summary>
+		/// Gets the primary key interface.
+		/// </summary>
+		/// <value>
+		/// The primary key interface.
+		/// </value>
+		public INamedTypeSymbol IPrimaryKey { get; }
 
-        /// <summary>
-        /// Gets the generic IPrimaryKeyOf<TDAC> interface.
-        /// </summary>
-        /// <value>
-        /// The generic IPrimaryKeyOf<TDAC> interface.
-        /// </value>
-        public INamedTypeSymbol IPrimaryKeyOf1 { get; }
+		/// <summary>
+		/// Gets the generic IPrimaryKeyOf<TDAC> interface.
+		/// </summary>
+		/// <value>
+		/// The generic IPrimaryKeyOf<TDAC> interface.
+		/// </value>
+		public INamedTypeSymbol IPrimaryKeyOf1 { get; }
 
-        /// <summary>
-        /// Gets the foreign key interface. For earlier versions of Acumatica (2019R1) is not defined so it can be null.
-        /// </summary>
-        /// <value>
-        /// The foreign key interface.
-        /// </value>
-        public INamedTypeSymbol IForeignKey { get; }
+		/// <summary>
+		/// Gets the foreign key interface. For earlier versions of Acumatica (2019R1) is not defined so it can be null.
+		/// </summary>
+		/// <value>
+		/// The foreign key interface.
+		/// </value>
+		public INamedTypeSymbol IForeignKey { get; }
 
-        /// <summary>
-        /// Gets the generic foreign key to the parent DAC interface derived from <see cref="IForeignKey"/>. Contains information about parent DAC referenced by the foreign key.
-        /// For earlier versions of Acumatica (2019R1) the interface is not defined so the symbol can be null.
-        /// </summary>
-        /// <value>
-        /// The generic foreign key to the parent DAC interface
-        /// </value>
-        public INamedTypeSymbol IForeignKeyTo1 { get; }
+		/// <summary>
+		/// Gets the generic foreign key to the parent DAC interface derived from <see cref="IForeignKey"/>. Contains information about parent DAC referenced by the foreign key.
+		/// For earlier versions of Acumatica (2019R1) the interface is not defined so the symbol can be null.
+		/// </summary>
+		/// <value>
+		/// The generic foreign key to the parent DAC interface
+		/// </value>
+		public INamedTypeSymbol IForeignKeyTo1 { get; }
 
-        public INamedTypeSymbol KeysRelation { get; }
+		public INamedTypeSymbol KeysRelation { get; }
 
-        public INamedTypeSymbol PrimaryKeyOf => Compilation.GetTypeByMetadataName(TypeFullNames.PrimaryKeyOf)!;
+		public INamedTypeSymbol PrimaryKeyOf => Compilation.GetTypeByMetadataName(TypeFullNames.PrimaryKeyOf)!;
 
-        public INamedTypeSymbol CompositeKey2 => Compilation.GetTypeByMetadataName(TypeFullNames.CompositeKey2)!;
+		public INamedTypeSymbol CompositeKey2 => Compilation.GetTypeByMetadataName(TypeFullNames.CompositeKey2)!;
 
-        internal PXReferentialIntegritySymbols(Compilation compilation) : base(compilation)
-        {
-            IPrimaryKey = Compilation.GetTypeByMetadataName(TypeFullNames.IPrimaryKey)!;
-            IPrimaryKeyOf1 = Compilation.GetTypeByMetadataName(TypeFullNames.IPrimaryKeyOf1)!;
+		internal PXReferentialIntegritySymbols(Compilation compilation) : base(compilation)
+		{
+			IPrimaryKey = Compilation.GetTypeByMetadataName(TypeFullNames.IPrimaryKey)!;
+			IPrimaryKeyOf1 = Compilation.GetTypeByMetadataName(TypeFullNames.IPrimaryKeyOf1)!;
 
-            IForeignKey = Compilation.GetTypeByMetadataName(TypeFullNames.IForeignKey)!;
-            IForeignKeyTo1 = Compilation.GetTypeByMetadataName(TypeFullNames.IForeignKeyTo1)!;
+			IForeignKey = Compilation.GetTypeByMetadataName(TypeFullNames.IForeignKey)!;
+			IForeignKeyTo1 = Compilation.GetTypeByMetadataName(TypeFullNames.IForeignKeyTo1)!;
 
-            KeysRelation = Compilation.GetTypeByMetadataName(TypeFullNames.KeysRelation)!;
-        }
+			KeysRelation = Compilation.GetTypeByMetadataName(TypeFullNames.KeysRelation)!;
+		}
 
-        public INamedTypeSymbol GetPrimaryKeyBy_TypeSymbol(int arity)
-        {
-            if (arity <= 0 || arity > MaxPrimaryKeySize)
-                throw new ArgumentOutOfRangeException(nameof(arity));
+		public INamedTypeSymbol GetPrimaryKeyBy_TypeSymbol(int arity)
+		{
+			if (arity <= 0 || arity > MaxPrimaryKeySize)
+				throw new ArgumentOutOfRangeException(nameof(arity));
 
-            string primaryKeyByTypeName = $"{TypeFullNames.PrimaryKeyOfBy}`{arity}";
-            return Compilation.GetTypeByMetadataName(primaryKeyByTypeName)!;
-        }
-    }
+			string primaryKeyByTypeName = $"{TypeFullNames.PrimaryKeyOfBy}`{arity}";
+			return Compilation.GetTypeByMetadataName(primaryKeyByTypeName)!;
+		}
+	}
 }
