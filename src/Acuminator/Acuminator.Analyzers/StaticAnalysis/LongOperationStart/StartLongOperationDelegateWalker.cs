a3822e28b0089d1cf5f7971369da82d1d618f621
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-03 03:40:17 +0100

    ATR-917: added support for ILongOperationManager to PX1086

--- src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationStart/StartLongOperationDelegateWalker.cs
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/LongOperationStart/StartLongOperationDelegateWalker.cs
@@ -1,11 +1,11 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 
 using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Constants;
 using Acuminator.Utilities.Roslyn.Semantic;
 using Acuminator.Utilities.Roslyn.Walkers;
 
@@ -31,32 +31,37 @@ namespace Acuminator.Analyzers.StaticAnalysis.LongOperationStart
 
 			IMethodSymbol? methodSymbol = GetSymbol<IMethodSymbol>(node);
 
-			if (methodSymbol == null || 
+			if (methodSymbol == null || node.ArgumentList?.Arguments.Count is null or 0 ||
 				!PxContext.AsyncOperations.StartOperation_AllMethods.Contains<IMethodSymbol>(methodSymbol, SymbolEqualityComparer.Default))
 			{
 				base.VisitInvocationExpression(node);
+				return;
 			}
 
-			var delegateExists = node.ArgumentList?.Arguments.Count > 1;
+			var delegateArgument = GetProcessingDelegateArgument(methodSymbol, node);
+
+			if (delegateArgument != null)
+			{
+				var delegateBody = GetDelegateNode(delegateArgument);
+
+				if (delegateBody != null)
+					_delegates.Add(delegateBody);
+			}
+		}
+
+		private ExpressionSyntax? GetProcessingDelegateArgument(IMethodSymbol methodSymbol, InvocationExpressionSyntax methodNode)
+		{
+			var arguments = methodNode.ArgumentList.Arguments;
+			var delegateExists = arguments.Count > 1;
 
 			if (!delegateExists)
-			{
-				return;
-			}
+				return null;
+			
+			var firstArgument = methodSymbol.Name == DelegateNames.Async.Await 
+				? arguments[0].Expression
+				: arguments[1].Expression;
 
-			var firstArgument = node.ArgumentList!.Arguments[1].Expression;
-			if (firstArgument == null)
-			{
-				return;
-			}
-
-			var delegateBody = GetDelegateNode(firstArgument);
-			if (delegateBody == null)
-			{
-				return;
-			}
-
-			_delegates.Add(delegateBody);
+			return firstArgument;
 		}
 	}
 }
