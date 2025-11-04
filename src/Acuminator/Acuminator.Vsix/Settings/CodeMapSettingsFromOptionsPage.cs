11c14b8117c17cf2c6a4c696e6312b57b7a186bb
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-04 02:54:04 +0100

    ATR-517: added settings wrapper for Code Map options page

---
+++ src/Acuminator/Acuminator.Vsix/Settings/CodeMapSettingsFromOptionsPage.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Acuminator.Utilities;
+using Acuminator.Utilities.Common;
+
+namespace Acuminator.Vsix.Settings
+{
+	[Export(typeof(CodeMapSettings))]
+	internal class CodeMapSettingsFromOptionsPage : CodeMapSettings
+	{
+		private readonly CodeMapOptionsPage _optionsPage;
+
+		[ImportingConstructor]
+		public CodeMapSettingsFromOptionsPage(CodeMapOptionsPage optionsPage)
+		{
+			_optionsPage = optionsPage.CheckIfNull();
+		}
+
+		public override bool ExpandRegularNodes => _optionsPage.ExpandRegularNodes;
+
+		public override bool ExpandAttributeNodes => _optionsPage.ExpandAttributeNodes;
+	}
+}
