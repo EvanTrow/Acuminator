732e04c4a39133466a07b9347bdf1bf22df0b0f9
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-06 19:46:44 +0100

    ATR-917: moved PX1013 unit tests to the rest of unit tests for the long runs in graphs, refactored the tests - fixed names fo files and tests

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/LongOperationStart/Sources/ActionHandlerReturnType/GraphWithActionDelegatesWithBadSignatures.cs
@@ -0,0 +1,31 @@
+﻿using PX.Data;
+using System;
+using System.Collections;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.ActionHandlerReturnType.Sources
+{
+	public class SMUserProcess : PXGraph
+	{
+		public PXAction<PX.SM.Users> SyncMyUsers = null!;
+		public PXAction<PX.SM.Users> DontSyncYsers = null!;         // check that action delegate with typo is not reported
+
+		[PXButton]
+		[PXUIField]
+		public void syncMyUsers()
+		{
+			SyncUsers();
+		}
+
+		[PXButton]
+		[PXUIField]
+		public IEnumerable dontSyncUsers()
+		{
+			yield return null;
+		}
+
+		private void SyncUsers()
+		{
+			PXLongOperation.StartOperation(this, () => Console.WriteLine("Synced"));
+		}
+	}
+}
