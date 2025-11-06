e7b4aecbbd6d18249df4e6b3701a0765a4dbcea6
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-07 00:22:30 +0100

    ATR-917: removed async and await from PX1086 tests

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInLongRunningOperationTests.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/ThrowingExceptions/ThrowingExceptionsInLongRunningOperationTests.cs
@@ -18,18 +18,21 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ThrowingExceptions
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
 			new PXGraphAnalyzer(
 				CodeAnalysisSettings.Default
-				.WithRecursiveAnalysisEnabled(),
+									.WithStaticAnalysisEnabled()
+									.WithRecursiveAnalysisEnabled()
+									.WithSuppressionMechanismDisabled(),
 				new ThrowingExceptionsInLongRunningOperationAnalyzer());
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\LongOperationStart_Bad.cs")]
-		public async Task LongOperationStart_ReportsDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source, Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(21, 13));
+		public Task LongOperationStart_ReportsDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source, 
+				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(21, 13));
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\ParametersDelegate_Bad.cs")]
-		public async Task ParametersDelegate_ReportsDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source,
+		public Task ParametersDelegate_ReportsDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(36, 21),
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(50, 21),
 				// With the current version of Roslyn packages 1.x we cannot analyze this construction
@@ -38,8 +41,8 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ThrowingExceptions
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\ProcessDelegate_Bad.cs")]
-		public async Task ProcessDelegate_ReportsDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source,
+		public Task ProcessDelegate_ReportsDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(29, 17),
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(33, 17),
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(35, 57),
@@ -51,8 +54,8 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ThrowingExceptions
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\FinallyProcessDelegate_Bad.cs")]
-		public async Task FinallyProcessDelegate_ReportsDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source,
+		public Task FinallyProcessDelegate_ReportsDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source,
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(30, 21),
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(34, 21),
 				Descriptors.PX1086_ThrowingSetupNotEnteredExceptionInLongRunningOperation.CreateFor(40, 21),
@@ -64,22 +67,22 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.ThrowingExceptions
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\LongOperationStart_Good.cs")]
-		public async Task LongOperationStart_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source);
+		public Task LongOperationStart_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\ParametersDelegate_Good.cs")]
-		public async Task ParametersDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source);
+		public Task ParametersDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\ProcessDelegate_Good.cs")]
-		public async Task ProcessDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source);
+		public Task ProcessDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
 
 		[Theory]
 		[EmbeddedFileData(@"LongOperations\FinallyProcessDelegate_Good.cs")]
-		public async Task FinallyProcessDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
-			await VerifyCSharpDiagnosticAsync(source);
+		public Task FinallyProcessDelegate_WithoutPXSetupNotEnteredException_DoesntReportDiagnostic(string source) =>
+			VerifyCSharpDiagnosticAsync(source);
 	}
 }
