8cb35a78c6833b2023c736a71eb689f4aeb7032f
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-11 17:34:24 +0100

    ATR-837: implemented basic draft for PX1120 analyzer

---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/IncorrectTaskUsageInAsyncCode/IncorrectTaskUsageInAsyncCodeAnalyzer.TaskUsageCheckingWalker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Utilities;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Acuminator.Analyzers.StaticAnalysis.IncorrectTaskUsageInAsyncCode
+{
+	public partial class IncorrectTaskUsageInAsyncCodeAnalyzer : PXDiagnosticAnalyzer
+	{
+		private class TaskUsageCheckingWalker : CSharpSyntaxWalker 
+		{
+			private readonly SyntaxNodeAnalysisContext _syntaxContext;
+			private readonly PXContext _pxContext;
+
+			public TaskUsageCheckingWalker(SyntaxNodeAnalysisContext syntaxContext, PXContext pxContext)
+			{
+				_syntaxContext = syntaxContext;
+				_pxContext	   = pxContext;
+			}
+		}
+	}
+}
---
+++ src/Acuminator/Acuminator.Analyzers/StaticAnalysis/IncorrectTaskUsageInAsyncCode/IncorrectTaskUsageInAsyncCodeAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Acuminator.Utilities;
+using Acuminator.Utilities.Roslyn.Semantic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Acuminator.Analyzers.StaticAnalysis.IncorrectTaskUsageInAsyncCode
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public partial class IncorrectTaskUsageInAsyncCodeAnalyzer : PXDiagnosticAnalyzer
+	{
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+			ImmutableArray.Create
+			(
+				Descriptors.PX1120_IncorrectTaskUsageInAsyncCode
+			);
+
+		public IncorrectTaskUsageInAsyncCodeAnalyzer() : this(null)
+		{ }
+
+		public IncorrectTaskUsageInAsyncCodeAnalyzer(CodeAnalysisSettings? codeAnalysisSettings) : base(codeAnalysisSettings)
+		{ }
+
+		protected override void AnalyzeCompilation(CompilationStartAnalysisContext compilationStartContext, PXContext pxContext)
+		{
+			compilationStartContext.RegisterSyntaxNodeAction(c => AnalyzeTaskUsageInTypes(c, pxContext), 
+															 SyntaxKind.CompilationUnit);
+		}
+
+		private static void AnalyzeTaskUsageInTypes(SyntaxNodeAnalysisContext syntaxContext, PXContext pxContext)
+		{
+			syntaxContext.CancellationToken.ThrowIfCancellationRequested();
+
+			if (syntaxContext.Node is not CompilationUnitSyntax compilationUnit)
+				return;
+
+			var tasksChecker	 = new TaskUsageCheckingWalker(syntaxContext, pxContext);
+			var typeDeclarations = compilationUnit.DescendantNodes()
+												  .OfType<TypeDeclarationSyntax>();
+
+			foreach (TypeDeclarationSyntax typeNode in typeDeclarations)
+			{
+				syntaxContext.CancellationToken.ThrowIfCancellationRequested();
+				tasksChecker.Visit(typeNode);
+			}
+		}
+	}
+}
