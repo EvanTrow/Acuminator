3c8d444f514798a421c6db243c00c2602c06c8b6
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-12 02:49:45 +0100

    ATR-837: added unit tests for new PX1120 diagnostic

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/IncorrectTaskUsageInAsyncCode/IncorrectTaskUsageInAsyncCodeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.IncorrectTaskUsageInAsyncCode;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+using AnalyzerResources = Acuminator.Analyzers.Resources;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.IncorrectTaskUsageInAsyncCode
+{
+	public class IncorrectTaskUsageInAsyncCodeTests : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new IncorrectTaskUsageInAsyncCodeAnalyzer(CodeAnalysisSettings.Default.WithStaticAnalysisEnabled()
+																				  .WithSuppressionMechanismDisabled());
+		
+		
+	}
+}
