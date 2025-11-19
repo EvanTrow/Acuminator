d5afdcee04f703364106d0a98260042de6e9805e
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-19 16:29:42 +0100

    ATR-780: added new complex test case for entity attributes

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacPropertyAttributes/Sources/DacWithPXDBCalcedAndUnboundEntityAttributes.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/DacPropertyAttributes/Sources/DacWithPXDBCalcedAndUnboundEntityAttributes.cs
@@ -16,6 +16,7 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.DacFieldWithDBCalcedAttribute.So
 		#region ComponentID
 		public abstract class componentID : BqlInt.Field<componentID> { }
 
+		// Entity Attribute has IsDBField set directly on the property
 		[Inventory(DisplayName = "Component ID", IsDBField = false)]
 		[PXDBCalced(typeof(Switch<Case<Where<ARTran.inventoryID, NotEqual<INTran.inventoryID>>, INTran.inventoryID>>), typeof(int))]
 		public virtual int? ComponentID
@@ -24,5 +25,25 @@ namespace Acuminator.Tests.Tests.StaticAnalysis.DacFieldWithDBCalcedAttribute.So
 			set;
 		}
 		#endregion
+
+		#region ComponentID1
+		public abstract class componentID1 : BqlInt.Field<componentID1> { }
+
+		// Entity Attribute has IsDBField set directly on the property
+		[MyEntityAttribute]
+		[PXDBCalced(typeof(Switch<Case<Where<ARTran.inventoryID, NotEqual<INTran.inventoryID>>, INTran.inventoryID>>), typeof(int))]
+		public virtual int? ComponentID1
+		{
+			get;
+			set;
+		}
+		#endregion
+	}
+
+
+	[Inventory(DisplayName = "Component ID", IsDBField = false)]
+	public class MyEntityAttribute : PXAggregateAttribute 
+	{
+
 	}
 }
