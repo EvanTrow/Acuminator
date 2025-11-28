7b1eb0fb6e7f5b2f6144a30e78495632dc04cf20
Sergey Nikomarov <snikomarov@acumatica.com>
2025-11-28 15:54:45 +0100

    ATR-892: reworked ExtensionCandidateInfo base type and implemented IInferredAcumaticaFrameworkTypeInfo with it

---
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Shared/Infer/ExtensionCandidateInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Acuminator.Utilities.Roslyn.Semantic.Shared.Infer
+{
+	public abstract class ExtensionCandidateInfo<TAcumaticaFrameworkType, TExtensionInfo> : NodeSymbolItem<ClassDeclarationSyntax, INamedTypeSymbol>, 
+																							IInferredAcumaticaFrameworkTypeInfo<TExtensionInfo>,
+																							IHaveDeclarationOrder
+	where TAcumaticaFrameworkType : NodeSymbolItem<ClassDeclarationSyntax, INamedTypeSymbol>
+	where TExtensionInfo : NodeSymbolItem<ClassDeclarationSyntax, INamedTypeSymbol>
+	{
+		protected internal List<TExtensionInfo> BaseExtensionsMutable { get; } =  new(capacity: 1);
+
+		public IReadOnlyCollection<TExtensionInfo> BaseExtensions => BaseExtensionsMutable;
+
+		protected internal List<TAcumaticaFrameworkType> RootTypesMutable { get; } = new(capacity: 1);
+
+		public IReadOnlyCollection<TAcumaticaFrameworkType> RootTypes => RootTypesMutable;
+
+		public bool HasCircularReferences 
+		{ 
+			get;
+			internal set;
+		}
+
+		public bool HasMultipleRootTypes => RootTypes.Count > 1;
+
+		protected ExtensionCandidateInfo(ClassDeclarationSyntax? extensionNode, INamedTypeSymbol extensionSymbol, int declarationOrder) :
+									base(extensionNode, extensionSymbol, declarationOrder)
+		{
+		}
+
+		public abstract TExtensionInfo? GetFrameworkTypeInfo();
+	}
+}
