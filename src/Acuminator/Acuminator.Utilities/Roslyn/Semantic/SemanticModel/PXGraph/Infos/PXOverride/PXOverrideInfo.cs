700584b141f1f3421574bd75eeac92b6011e71b5
Sergey Nikomarov <snikomarov@acumatica.com>
2025-12-05 00:26:05 +0100

    ATR-892: fixed the collection of declared PXOverride infos and added detection of ambigous  cases with multiple suitable base methods for the PXOverride patch method

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Infos/PXOverride/PXOverrideInfo.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/PXGraph/Infos/PXOverride/PXOverrideInfo.cs
@@ -42,9 +42,9 @@ namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 
 			var directBaseTypesAndThis = graphExtensionInfo.Symbol.GetBaseTypesAndThis()
 																  .ToList(capacity: 4);
-			var graphAndGraphExtensionBaseTypes = graphExtensionInfo.JustOverridenItems()
+			var graphAndGraphExtensionBaseTypes = graphExtensionInfo.GetInfosFromDerivedExtensionToBaseGraph(includeSelf: false)
 																	.Select(info => info.Symbol)
-																	.Distinct<INamedTypeSymbol>(SymbolEqualityComparer.Default)
+																	.Distinct<ITypeSymbol>(SymbolEqualityComparer.Default)
 																	.Where(baseType => !directBaseTypesAndThis.Contains(baseType, SymbolEqualityComparer.Default))
 																	.ToList(capacity: 4);
 
@@ -69,22 +69,37 @@ namespace Acuminator.Utilities.Roslyn.Semantic.PXGraph
 			}
 		}
 
-		private static IMethodSymbol? GetBaseMethod(IMethodSymbol patchMethodWithPXOverride, List<INamedTypeSymbol> graphAndGraphExtensionBaseTypes,
+		private static IMethodSymbol? GetBaseMethod(IMethodSymbol patchMethodWithPXOverride, List<ITypeSymbol> graphAndGraphExtensionBaseTypes,
 													INamedTypeSymbol pxOverrideAttribute)
 		{
+			var suitableBaseMethods = new List<IMethodSymbol>(capacity: 4);
+
 			foreach (var baseType in graphAndGraphExtensionBaseTypes)
 			{
-				// Base method search should look for the first suitable method in the base type hierarchy from the most derived type to the least derived type.
-				// The base method is the first one with the suitable signature that is not marked with PXOverrideAttribute.
+				// Base method search should look for suitable methods in the base type hierarchy from the most derived type to the least derived type.
+				// The base method is the one with the suitable signature that is not marked with PXOverrideAttribute.
 				// The additional condition is required to filter out overrides of the base method in base extensions.
 				var baseMethod = baseType.GetMethods(patchMethodWithPXOverride.Name)
-												 .FirstOrDefault(baseMethod => patchMethodWithPXOverride.IsPXOverrideOf(baseMethod) && 
-																			   !baseMethod.HasPXOverrideAttribute(pxOverrideAttribute));
+										 .FirstOrDefault(baseMethod => patchMethodWithPXOverride.IsPXOverrideOf(baseMethod) && 
+																	  !baseMethod.HasPXOverrideAttribute(pxOverrideAttribute));
 				if (baseMethod != null)
-					return baseMethod;
+				{
+					bool alreadyFoundCSharpOverride = 
+						suitableBaseMethods.Count > 0 && 
+						suitableBaseMethods.Any(foundMethod => foundMethod.IsOverride && 
+															   foundMethod.GetOverridden().Contains(baseMethod, SymbolEqualityComparer.Default));
+
+					// Do not add the base method if a more derived C# override is already found
+					if (!alreadyFoundCSharpOverride)
+						suitableBaseMethods.Add(baseMethod);
+				}
 			}
 
-			return null;
+			// If there are multiple suitable base methods found, we cannot determine which one is the correct base method for PXOverride
+			// Thus, we return null in this case
+			return suitableBaseMethods.Count == 1
+				? suitableBaseMethods[0]
+				: null;
 		}
 
 		private static PXOverrideType GetPXOverrideType(IMethodSymbol patchMethodWithPXOverride, IMethodSymbol? baseMethod)
