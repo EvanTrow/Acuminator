b18c85b5f028b495ad0db39d62a52d4aa4992888
Sergey Nikomarov <snikomarov@acumatica.com>
2025-12-04 12:58:15 +0100

    ATR-892: removed redundant old creation algorithms from DAC and DAC Extension infos

--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Infos/DacOrDacExt/DacExtensionInfo.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Infos/DacOrDacExt/DacExtensionInfo.cs
@@ -3,7 +3,6 @@ using System.Threading;
 
 using Acuminator.Utilities.Common;
 using Acuminator.Utilities.Roslyn.Semantic.Shared;
-using Acuminator.Utilities.Roslyn.Syntax;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -30,40 +29,6 @@ namespace Acuminator.Utilities.Roslyn.Semantic.Dac
 		{
 			BaseExtensionsMechanismType = ExtensionMechanismType.None;
 			Dac = dac;
-		}
-
-		public static DacExtensionInfo? Create(ITypeSymbol? dacExtension, ClassDeclarationSyntax? dacExtensionNode, ITypeSymbol? dac, 
-											   PXContext pxContext, int dacExtDeclarationOrder, CancellationToken cancellation)
-		{
-			if (dacExtension == null)
-				return null;
-
-			cancellation.ThrowIfCancellationRequested();
-
-			var dacNode = dac.GetSyntax(cancellation) as ClassDeclarationSyntax;
-			var dacInfo = DacInfo.Create(dac, dacNode, pxContext, dacDeclarationOrder: 0, cancellation);
-			
-			var extensionTypesFromFirstToLastLevel = dacExtension.GetBaseExtensions(pxContext, SortDirection.Ascending, includeDac: false);
-			DacExtensionInfo? aggregatedBaseDacInfo = null, prevDacInfo = null;
-
-			foreach (ITypeSymbol baseExtensionType in extensionTypesFromFirstToLastLevel)
-			{
-				cancellation.ThrowIfCancellationRequested();
-
-				var baseDacExtNode = baseExtensionType.GetSyntax(cancellation) as ClassDeclarationSyntax;
-
-				aggregatedBaseDacInfo = prevDacInfo != null
-					? new DacExtensionInfo(baseDacExtNode, baseExtensionType, dacInfo, declarationOrder: 1, prevDacInfo)
-					: new DacExtensionInfo(baseDacExtNode, baseExtensionType, dacInfo, declarationOrder: 1);
-
-				prevDacInfo = aggregatedBaseDacInfo;
-			}
-
-			var dacExtensionInfo = aggregatedBaseDacInfo != null
-				? new DacExtensionInfo(dacExtensionNode, dacExtension, dacInfo, dacExtDeclarationOrder, aggregatedBaseDacInfo)
-				: new DacExtensionInfo(dacExtensionNode, dacExtension, dacInfo, dacExtDeclarationOrder);
-
-			return dacExtensionInfo;
 		}
 	}
 }
--- src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Infos/DacOrDacExt/DacInfo.cs
+++ src/Acuminator/Acuminator.Utilities/Roslyn/Semantic/SemanticModel/Dac/Infos/DacOrDacExt/DacInfo.cs
@@ -5,8 +5,6 @@ using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-using Acuminator.Utilities.Roslyn.Syntax;
-
 namespace Acuminator.Utilities.Roslyn.Semantic.Dac
 {
 	public class DacInfo : DacOrDacExtInfoBase<DacInfo>
@@ -19,42 +17,6 @@ namespace Acuminator.Utilities.Roslyn.Semantic.Dac
 		internal DacInfo(ClassDeclarationSyntax? node, ITypeSymbol dac, int declarationOrder) :
 					base(node, dac, declarationOrder)
 		{
-		}
-
-		public static DacInfo? Create(ITypeSymbol? dac, ClassDeclarationSyntax? dacNode, PXContext pxContext, 
-									  int dacDeclarationOrder, CancellationToken cancellation)
-		{
-			if (dac == null)
-				return null;
-
-			cancellation.ThrowIfCancellationRequested();
-
-			var dacBaseTypesFromBaseToDerived = dac.GetDacBaseTypesThatMayStoreDacProperties(pxContext)
-												   .Reverse();
-			bool isInSource = dacNode != null;
-			DacInfo? aggregatedBaseDacInfo = null, prevDacInfo = null;
-
-			foreach (ITypeSymbol baseType in dacBaseTypesFromBaseToDerived)
-			{
-				cancellation.ThrowIfCancellationRequested();
-
-				var baseDacNode = isInSource
-					? baseType.GetSyntax(cancellation) as ClassDeclarationSyntax
-					: null;
-
-				isInSource = baseDacNode != null;
-				aggregatedBaseDacInfo = prevDacInfo != null 
-					? new DacInfo(baseDacNode, baseType, declarationOrder: 0, prevDacInfo)
-					: new DacInfo(baseDacNode, baseType, declarationOrder: 0);
-
-				prevDacInfo = aggregatedBaseDacInfo;
-			}
-
-			var dacInfo = aggregatedBaseDacInfo != null
-				? new DacInfo(dacNode, dac, dacDeclarationOrder, aggregatedBaseDacInfo)
-				: new DacInfo(dacNode, dac, dacDeclarationOrder);
-
-			return dacInfo;
 		}
 	}
 }
