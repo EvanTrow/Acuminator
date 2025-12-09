b9ba50a9ffbd00465132cbdfaceaca83ccf9b9ac
Sergey Nikomarov <snikomarov@acumatica.com>
2025-12-10 00:15:34 +0100

    ATR-820: added unit test for circular DAC extensions

---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/CircularDacExtensionReferencesTests.cs
@@ -0,0 +1,61 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities.Common;
+using Acuminator.Utilities.Roslyn.Semantic.Dac;
+using Acuminator.Utilities.Roslyn.Semantic.Shared.Infer;
+using Acuminator.Utilities.Roslyn.Semantic.Shared.Infer.Dac;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Dac
+{
+	public class CircularDacExtensionReferencesTests : SemanticModelTestsBase<DacSemanticModel>
+	{
+		[Theory]
+		[EmbeddedFileData("DacExtensionWithCircularReference.cs")]
+		public async Task DacExtension_WithCircularReference_Trivial(string text)
+		{
+			var testContext = await PrepareTestContextForCodeAsync(text).ConfigureAwait(false);
+
+			var dacSemanticModel = await PrepareSemanticModelAsync(testContext, cancellation: default).ConfigureAwait(false);
+			dacSemanticModel.Should().BeNull();
+		}
+
+#pragma warning disable CS8609 // Nullability of reference types in return type doesn't match overridden member.
+		protected override Task<DacSemanticModel?> PrepareSemanticModelAsync(RoslynTestContext context, CancellationToken cancellation)
+		{
+			var dacOrDacExtDeclaration = context.Root.DescendantNodes()
+													 .OfType<ClassDeclarationSyntax>()
+													 .FirstOrDefault();
+			dacOrDacExtDeclaration.Should().NotBeNull();
+
+			INamedTypeSymbol? dacOrDacExtSymbol = context.SemanticModel.GetDeclaredSymbol(dacOrDacExtDeclaration, cancellation);
+			dacOrDacExtDeclaration.Should().NotBeNull();
+
+			var inferredInfo = DacAndDacExtInfoBuilder.Instance.InferTypeInfo(dacOrDacExtSymbol!, context.PXContext, customDeclarationOrder: null, 
+																			  cancellation);
+			inferredInfo.Should().NotBeNull();
+			inferredInfo!.InferredInfo.Should().BeNull();
+			inferredInfo.GetResultKind().Should().Be(InferResultKind.CircularReferences);
+			inferredInfo.CircularReferenceExtension.Should().NotBeNull();
+			inferredInfo.CircularReferenceExtension!.Should().Be(dacOrDacExtSymbol);
+
+			return Task.FromResult<DacSemanticModel?>(null);
+		}
+#pragma warning restore CS8609
+	}
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/Utilities/SemanticModels/Dac/Sources/DacExtensionWithCircularReference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PX.Data;
+
+namespace Acuminator.Tests.Tests.Utilities.SemanticModels.Graph.Sources
+{
+	/// <exclude/>
+	public class Extension1 : PXCacheExtension<Extension2, MyDac> { }
+
+	/// <exclude/>
+	public class Extension2 : PXCacheExtension<Extension1, MyDac> { }
+
+	[PXHidden]
+	public class MyDac : PXBqlTable, IBqlTable
+	{ }
+}
