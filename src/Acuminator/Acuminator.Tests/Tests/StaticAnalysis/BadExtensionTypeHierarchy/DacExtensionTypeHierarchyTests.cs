e5104dbb9ea28ad3430eafd28b759d163d9b61d8
Sergey Nikomarov <snikomarov@acumatica.com>
2025-12-10 04:26:25 +0100

    ATR-820: added unit tests for graph extension  and DAC extension type hierarchy

---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BadExtensionTypeHierarchy/DacExtensionTypeHierarchyTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.Dac;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy;
+
+public class DacExtensionTypeHierarchyTests : DiagnosticVerifier
+{
+	protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new DacAnalyzersAggregator(CodeAnalysisSettings.Default
+														   .WithStaticAnalysisEnabled()
+														   .WithSuppressionMechanismDisabled());
+
+	[Theory]
+	[EmbeddedFileData(@"Dac\DacExtension_LinearHierarchy.cs")]
+	public Task DacExtension_WithLinearHierarchy_NoDiagnostic(string actual) =>
+		VerifyCSharpDiagnosticAsync(actual);
+
+	[Theory]
+	[EmbeddedFileData(@"Dac\DacExtensionWithComplexCircularReference.cs")]
+	public Task DacExtension_WithCircularReferences_InTypeHierarchy(string actual) =>
+		VerifyCSharpDiagnosticAsync(actual, 
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_DacExtension.CreateFor(11, 22,
+											["ExtensionA", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Dac.Sources.ExtensionA"]),
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_DacExtension.CreateFor(15, 22,
+											["ExtensionB", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Dac.Sources.ExtensionB"]),
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_DacExtension.CreateFor(19, 22,
+											["ExtensionC", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Dac.Sources.ExtensionC"]));
+
+
+}
---
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/BadExtensionTypeHierarchy/GraphExtensionTypeHierarchyTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Acuminator.Analyzers.StaticAnalysis;
+using Acuminator.Analyzers.StaticAnalysis.PXGraph;
+using Acuminator.Tests.Helpers;
+using Acuminator.Tests.Verification;
+using Acuminator.Utilities;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using Xunit;
+
+namespace Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy;
+
+public class GraphExtensionTypeHierarchyTests : DiagnosticVerifier
+{
+	protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() =>
+			new PXGraphAnalyzer(CodeAnalysisSettings.Default
+													.WithStaticAnalysisEnabled()
+													.WithSuppressionMechanismDisabled());
+
+	[Theory]
+	[EmbeddedFileData(@"Graph\GraphExtension_LinearHierarchy.cs")]
+	public Task GraphExtension_WithLinearHierarchy_NoDiagnostic(string actual) =>
+		VerifyCSharpDiagnosticAsync(actual);
+
+	[Theory]
+	[EmbeddedFileData(@"Graph\GraphExtension_ComplexHierarchy.cs")]
+	public Task GraphExtension_WithComplexHierarchy_NoDiagnostic(string actual) =>
+		VerifyCSharpDiagnosticAsync(actual);
+
+	[Theory]
+	[EmbeddedFileData(@"Graph\GraphExtensionWithComplexCircularReference.cs")]
+	public Task GraphExtension_WithCircularReferences_InTypeHierarchy(string actual) =>
+		VerifyCSharpDiagnosticAsync(actual, 
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_GraphExtension.CreateFor(10, 15,
+											["ExtensionA", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Graph.Sources.ExtensionA"]),
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_GraphExtension.CreateFor(13, 15,
+											["ExtensionB", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Graph.Sources.ExtensionB"]),
+			Descriptors.PX1116_CircularReferenceInTypeHierarchy_GraphExtension.CreateFor(16, 15,
+											["ExtensionC", "Acuminator.Tests.Tests.StaticAnalysis.BadExtensionTypeHierarchy.Graph.Sources.ExtensionC"]));
+
+
+}
