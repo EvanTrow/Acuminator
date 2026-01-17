fbdb77f2173dc7a01dcc406142fc786ea89d24a6
Sergey Nikomarov <snikomarov@acumatica.com>
2026-01-17 21:49:16 +0300

    ATR-941: added recognition of base properties and events referenced in the XML doc comment to the PX1098 diagnostic

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/XmlComment/PXOverrideOfPropertyFromBasePXGraphWithoutXmlComment.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/XmlComment/PXOverrideOfPropertyFromBasePXGraphWithoutXmlComment.cs
@@ -8,6 +8,7 @@ namespace Acuminator.Tests.Sources
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public class BaseExtension : PXGraphExtension<MyGraph>
 	{
+
 		[PXOverride]
 		[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Test sources")]
 		public Type get_PrimaryItemType(Func<Type> base_PrimaryItemType) => typeof(MyDac);
--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/XmlComment/PXOverrideOfPropertyFromBasePXGraphWithoutXmlComment_Expected.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/XmlComment/PXOverrideOfPropertyFromBasePXGraphWithoutXmlComment_Expected.cs
@@ -8,6 +8,7 @@ namespace Acuminator.Tests.Sources
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public class BaseExtension : PXGraphExtension<MyGraph>
 	{
+
 		/// Overrides <seealso cref="PXGraph.PrimaryItemType"/>
 		[PXOverride]
 		[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Test sources")]
