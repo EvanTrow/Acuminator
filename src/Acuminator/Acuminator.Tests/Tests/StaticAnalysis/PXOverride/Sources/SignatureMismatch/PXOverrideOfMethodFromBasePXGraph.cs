cc0135f9246bfeb7484bd914324286e532b103c0
Sergey Nikomarov <snikomarov@acumatica.com>
2026-01-17 16:51:26 +0300

    ATR-941: update test source for PX1096 to include an overridden graph property

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/SignatureMismatch/PXOverrideOfMethodFromBasePXGraph.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/SignatureMismatch/PXOverrideOfMethodFromBasePXGraph.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
 using PX.Data;
 
 namespace Acuminator.Tests.Sources
@@ -19,9 +21,19 @@ namespace Acuminator.Tests.Sources
 		{
 			base_Clear();
 		}
+
+		/// Overrides <seealso cref="PXGraph.PrimaryItemType"/>
+		[PXOverride]
+		[SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Test sources")]
+		public Type get_PrimaryItemType(Func<Type> base_PrimaryItemType) => typeof(MyDac);
 	}
 
 	public class MyGraph : PXGraph<MyGraph> 
+	{
+	}
+
+	[PXHidden]
+	public class MyDac : PXBqlTable, IBqlTable
 	{
 	}
 }
