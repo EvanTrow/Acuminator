d18b51fa06812f443ca0b7eecefdb4ca9f75376c
Sergey Nikomarov <snikomarov@acumatica.com>
2026-01-19 03:24:24 +0300

    ATR-943: added unit tests for PX1102 with ref/out/in/ref readonly in method signatures

--- src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/BaseDelegateParameter/InvalidParameterName/PXOverrideRefAndOutParametersWithIncorrectDelegateParameterName_Expected.cs
+++ src/Acuminator/Acuminator.Tests/Tests/StaticAnalysis/PXOverride/Sources/BaseDelegateParameter/InvalidParameterName/PXOverrideRefAndOutParametersWithIncorrectDelegateParameterName_Expected.cs
@@ -9,16 +9,16 @@ namespace Acuminator.Tests.Sources
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public class APReleaseProcessDatasourceExt : PXGraphExtension<MyGraph>
 	{
-		public delegate void DoSomething1DelegateType(ref MyDac doc, bool isPrebooking, List<MyDac> inDocs);
+		public delegate void DoSomething1DelegateType(ref MyDac doc, bool isPrebooking, out List<MyDac> inDocs);
 
 		[PXOverride]
 		public void DoSomething1(ref MyDac doc, bool isPrebooking, out List<MyDac> inDocs, DoSomething1DelegateType base_DoSomething1)
 		{
 			inDocs = new List<MyDac>();
-			base_DoSomething1(ref doc, isPrebooking, inDocs);
+			base_DoSomething1(ref doc, isPrebooking, out inDocs);
 		}
 
-		public delegate void DoSomething2DelegateType(ref MyDac doc, in bool isPrebooking, out List<MyDac> inDocs);
+		public delegate void DoSomething2DelegateType(ref MyDac doc, bool isPrebooking, out List<MyDac> inDocs);
 
 		[PXOverride]
 		public void DoSomething2(ref MyDac doc, bool isPrebooking, out List<MyDac> inDocs, DoSomething2DelegateType base_DoSomething2)
@@ -27,7 +27,7 @@ namespace Acuminator.Tests.Sources
 			base_DoSomething2(ref doc, isPrebooking, out inDocs);
 		}
 
-		public delegate ref bool DoSomething3DelegateType(MyDac doc);
+		public delegate ref readonly bool DoSomething3DelegateType(MyDac doc);
 
 		[PXOverride]
 		public ref readonly bool DoSomething3(MyDac doc, DoSomething3DelegateType base_DoSomething3) =>
